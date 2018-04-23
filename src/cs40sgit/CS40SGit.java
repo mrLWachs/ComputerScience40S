a7f689d395d836cb2f26f37249460f9dd112d523
Mr. Wachs <mrLWachs@gmail.com>
2018-04-23 15:27:38 -0500

    In class examples

---
+++ src/cs40sgit/CS40SGit.java
@@ -0,0 +1,26 @@
+
+/** required package class namespace */
+package cs40sgit;
+
+/**
+ * CS40SGit.java - description here...
+ *
+ * @author lawrence.wachs 
+ * @since Apr 23, 2018 
+ * @instructor Mr. Wachs
+ */
+public class CS40SGit 
+{
+
+    /**
+     * Main method for the project
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+    }
+
+}
