c33436c0f7a8eeb1696c0921a20541b2503cba9a
Mr. Wachs <mrLWachs@gmail.com>
2018-09-24 10:54:07 -0500

    New 2018-2019 School Year

---
+++ src/testing/Tester.java
@@ -0,0 +1,23 @@
+
+
+/** required package class namespace */
+package testing;
+
+
+
+/**
+ * Tester.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 24-Sep-2018 
+ */
+public class Tester 
+{
+
+    public Tester() {
+        System.out.println("Testing begins.....");        
+        RecursionTest unit1 = new RecursionTest();        
+        System.out.println("Testing complete!");
+    }
+    
+}
