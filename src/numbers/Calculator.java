5002722dfeda9b2930e65a50ac76afc7aea8d84a
Mr. Wachs <mrLWachs@gmail.com>
2018-09-24 10:53:53 -0500

    New 2018-2019 School Year

---
+++ src/numbers/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ *  Mr. Wachs' classes - Java Computer Science learning file Calculator.java
+ *  for project ComputerScience40S on 24-Sep-2018 at 10:31:42 AM by lawrence.wachs
+ */
+
+
+/** required package class namespace */
+package numbers;
+
+
+
+/**
+ * Calculator.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 24-Sep-2018 
+ */
+public class Calculator 
+{
+    
+    public int factorial(int number) {
+        return 0;
+    }
+    
+}
