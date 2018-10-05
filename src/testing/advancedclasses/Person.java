edea0d2a43a2dffb42f6c2596c10f5b062a3fae3
Mr. Wachs <mrLWachs@gmail.com>
2018-10-05 11:08:46 -0500

    New 2018-2019 School Year

--- src/testing/advancedclasses/Person.java
+++ src/testing/advancedclasses/Person.java
@@ -11,5 +11,25 @@ package testing.advancedclasses;
  */
 public class Person 
 {
+    
+    private String name;    // encapsulated property
+    public int age;         // property
+    
+    /**
+     * Default constructor for the class (constructor methods use the same 
+     * name as the class and has NO return type - not even void)
+     */
+    public Person() {
+        name = "Jane doe";
+        age  = 0;
+    }
+    
+    /**
+     * Action method to have the person talk (outputs data on this person 
+     * to the screen)
+     */
+    public void talk() {
+        System.out.println("Person " + name + " is " + age);
+    }
 
 }
---
+++ src/testing/advancedclasses/Student.java
@@ -0,0 +1,17 @@
+
+/** required package class namespace */
+package testing.advancedclasses;
+
+
+/**
+ * Student.java - represents a student
+ *
+ * @author Mr. Wachs 
+ * @since 5-Oct-2018 
+ */
+public class Student extends Person
+{
+
+    public int studentNumber;
+    
+}
