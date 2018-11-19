44fb9491f6ef1141d25f58f89927ccc5c3ac2924
Mr. Wachs <mrLWachs@gmail.com>
2018-11-19 15:30:44 -0600

    New 2018-2019 School Year

---
+++ src/computerscience30s/Person.java
@@ -0,0 +1,21 @@
+
+
+/** required package class namespace */
+package computerscience30s;
+
+
+
+/**
+ * Person.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 19-Nov-2018 
+ */
+public class Person 
+{
+    // properties:
+    
+    
+    // Methods:
+    
+}
