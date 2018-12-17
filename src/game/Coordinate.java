c8cd85572db2d0013fdf174b03a07fa9c55c8608
Mr. Wachs <mrLWachs@gmail.com>
2018-12-17 10:49:15 -0600

    New 2018-2019 School Year

---
+++ src/game/Coordinate.java
@@ -0,0 +1,20 @@
+
+
+
+/** required package class namespace */
+package game;
+
+
+
+/**
+ * Coordinate.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 17-Dec-2018 
+ */
+public class Coordinate 
+{
+
+   
+
+}
