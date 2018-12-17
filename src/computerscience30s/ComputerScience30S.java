66473b3e8702d5534a6562442745a262024d3bbf
Mr. Wachs <mrLWachs@gmail.com>
2018-12-17 10:46:26 -0600

    New 2018-2019 School Year

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -2,6 +2,7 @@
 package computerscience30s;
 
 /** required imports */
+import game.Game;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.Icon;
@@ -34,7 +35,8 @@ public class ComputerScience30S
 //        unit2();    // methods in java
 //        unit3();    // arrays in java
 //        unit4();    // classes in java
-        unit5();    // graphics in java
+//        unit5();    // graphics in java
+        Game game = new Game();
         System.out.println("\nComputer Science 30S Example completed!\n");
     }
     
---
+++ src/game/Game.java
@@ -0,0 +1,20 @@
+
+/** required package class namespace */
+package game;
+
+
+
+/**
+ * Game.java - 
+ *
+ * @author Mr. Wachs 
+ * @since 17-Dec-2018 
+ */
+public class Game 
+{
+
+    public Game() {
+        System.out.println("test");
+    }
+
+}
