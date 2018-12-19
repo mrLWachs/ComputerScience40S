9cb8f3ec4610020f573f4549e58b52e70ff1015f
Mr. Wachs <mrLWachs@gmail.com>
2018-12-19 08:05:10 -0600

    New 2018-2019 School Year

--- src/game/Directions.java
+++ src/game/Directions.java
@@ -21,22 +21,24 @@ public class Directions
     // no directional movement
     public static final int STOP = 0;
     
-    // 16 or 8 directional game constants
+    // 8 directional game constants
     public static final int NORTH            = 1;
-    public static final int NORTH_NORTH_EAST = 2;    
-    public static final int NORTH_EAST       = 3;
-    public static final int EAST_NORTH_EAST  = 4;    
-    public static final int EAST             = 5;
-    public static final int EAST_SOUTH_EAST  = 6;    
-    public static final int SOUTH_EAST       = 7;
-    public static final int SOUTH_SOUTH_EAST = 8;    
-    public static final int SOUTH            = 9;
-    public static final int SOUTH_SOUTH_WEST = 10;    
-    public static final int SOUTH_WEST       = 11;
-    public static final int WEST_SOUTH_WEST  = 12;    
-    public static final int WEST             = 13;
-    public static final int WEST_NORTH_WEST  = 14;    
-    public static final int NORTH_WEST       = 15;
+    public static final int NORTH_EAST       = 2;
+    public static final int EAST             = 3;
+    public static final int SOUTH_EAST       = 4;
+    public static final int SOUTH            = 5;
+    public static final int SOUTH_WEST       = 6;
+    public static final int WEST             = 7;
+    public static final int NORTH_WEST       = 8;
+    
+    // 16 directional game constants
+    public static final int WEST_NORTH_WEST  = 9;
+    public static final int WEST_SOUTH_WEST  = 10;
+    public static final int SOUTH_SOUTH_WEST = 11;
+    public static final int SOUTH_SOUTH_EAST = 12;
+    public static final int EAST_SOUTH_EAST  = 13;
+    public static final int EAST_NORTH_EAST  = 14;
+    public static final int NORTH_NORTH_EAST = 15; 
     public static final int NORTH_NORTH_WEST = 16;
     
     // 4 or 2 directional game constants
