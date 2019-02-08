d12db4023269f2474bffd8ed1b2635965d361d8e
Mr. Wachs <mrLWachs@gmail.com>
2019-02-08 13:34:59 -0600

    New 2018-2019 School Year

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -21,106 +21,106 @@ public class ComputerScience30S
         */
         
         // Basic output
-        System.out.print("Hello World");    // comments can also go after code
-        
-        // Words starting with captial letters are powerful, they are a "class"
-        
-        // TIP: type "sout" and press TAB        
-        System.out.println("I'm in the world");
-        System.out.println("Good-bye world");
-        
-        // Output "special" characters
-        System.out.println("Quote \" " );
-        System.out.println("\tMake a tab");
-        System.out.println("\\Slash");
-        System.out.println("New\n\nLine");
-        
-        
-        // TIP: if your want intellisense to appear at any time press:
-        // "CTRL" + "SPACE"        
-        // TIP: "null" is a placeholder for code you don't actually want
-        
-        // Create "fancy" ouptut using a dialog
-        JOptionPane.showMessageDialog(null, "Hi");
-        
-        // Optional versions of output dialogs:
-        
-        JOptionPane.showMessageDialog(null, 
-                "Information dialog icon is i",
-                "Information Title",
-                JOptionPane.INFORMATION_MESSAGE);
-        
-        JOptionPane.showMessageDialog(null, 
-                "Question dialog icon is ?",
-                "Question Title",
-                JOptionPane.QUESTION_MESSAGE);
-                
-        JOptionPane.showMessageDialog(null, 
-                "Warning dialog icon is !",
-                "Warning Title",
-                JOptionPane.WARNING_MESSAGE);
-        
-        JOptionPane.showMessageDialog(null, 
-                "Plain dialog has no icon",
-                "Plain Title",
-                JOptionPane.PLAIN_MESSAGE);
-        
-        // dialog for inputs
-        
-        JOptionPane.showInputDialog("Enter something");
-        
-        // some optional versions of input dialog
-        
-        JOptionPane.showInputDialog(null, 
-                "Information dialog icon is i",
-                "Information Title",
-                JOptionPane.INFORMATION_MESSAGE);
-        
-        JOptionPane.showInputDialog(null, 
-                "Question dialog icon is ?",
-                "Question Title",
-                JOptionPane.QUESTION_MESSAGE);
-                
-        JOptionPane.showInputDialog(null, 
-                "Warning dialog icon is !",
-                "Warning Title",
-                JOptionPane.WARNING_MESSAGE);
-        
-        JOptionPane.showInputDialog(null, 
-                "Plain dialog has no icon",
-                "Plain Title",
-                JOptionPane.PLAIN_MESSAGE);
-        
-        // optional confirm dialog:
-        
-        JOptionPane.showConfirmDialog(null, "Confirm");
-        
-        // more optional examples of confirm dialog
-        
-        JOptionPane.showConfirmDialog(null,
-                "Example 1",
-                "Information",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.INFORMATION_MESSAGE);
-        
-        JOptionPane.showConfirmDialog(null,
-                "Example 2",
-                "Question",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE);
-        
-        JOptionPane.showConfirmDialog(null,
-                "Example 3",
-                "Warning",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.WARNING_MESSAGE);
-        
-        JOptionPane.showConfirmDialog(null,
-                "Example 4",
-                "Plain",
-                JOptionPane.OK_CANCEL_OPTION,
-                JOptionPane.PLAIN_MESSAGE);
-        
+//        System.out.print("Hello World");    // comments can also go after code
+//        
+//        // Words starting with captial letters are powerful, they are a "class"
+//        
+//        // TIP: type "sout" and press TAB        
+//        System.out.println("I'm in the world");
+//        System.out.println("Good-bye world");
+//        
+//        // Output "special" characters
+//        System.out.println("Quote \" " );
+//        System.out.println("\tMake a tab");
+//        System.out.println("\\Slash");
+//        System.out.println("New\n\nLine");
+//        
+//        
+//        // TIP: if your want intellisense to appear at any time press:
+//        // "CTRL" + "SPACE"        
+//        // TIP: "null" is a placeholder for code you don't actually want
+//        
+//        // Create "fancy" ouptut using a dialog
+//        JOptionPane.showMessageDialog(null, "Hi");
+//        
+//        // Optional versions of output dialogs:
+//        
+//        JOptionPane.showMessageDialog(null, 
+//                "Information dialog icon is i",
+//                "Information Title",
+//                JOptionPane.INFORMATION_MESSAGE);
+//        
+//        JOptionPane.showMessageDialog(null, 
+//                "Question dialog icon is ?",
+//                "Question Title",
+//                JOptionPane.QUESTION_MESSAGE);
+//                
+//        JOptionPane.showMessageDialog(null, 
+//                "Warning dialog icon is !",
+//                "Warning Title",
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showMessageDialog(null, 
+//                "Plain dialog has no icon",
+//                "Plain Title",
+//                JOptionPane.PLAIN_MESSAGE);
+//        
+//        // dialog for inputs
+//        
+//        JOptionPane.showInputDialog("Enter something");
+//        
+//        // some optional versions of input dialog
+//        
+//        JOptionPane.showInputDialog(null, 
+//                "Information dialog icon is i",
+//                "Information Title",
+//                JOptionPane.INFORMATION_MESSAGE);
+//        
+//        JOptionPane.showInputDialog(null, 
+//                "Question dialog icon is ?",
+//                "Question Title",
+//                JOptionPane.QUESTION_MESSAGE);
+//                
+//        JOptionPane.showInputDialog(null, 
+//                "Warning dialog icon is !",
+//                "Warning Title",
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showInputDialog(null, 
+//                "Plain dialog has no icon",
+//                "Plain Title",
+//                JOptionPane.PLAIN_MESSAGE);
+//        
+//        // optional confirm dialog:
+//        
+//        JOptionPane.showConfirmDialog(null, "Confirm");
+//        
+//        // more optional examples of confirm dialog
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 1",
+//                "Information",
+//                JOptionPane.DEFAULT_OPTION,
+//                JOptionPane.INFORMATION_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 2",
+//                "Question",
+//                JOptionPane.YES_NO_CANCEL_OPTION,
+//                JOptionPane.QUESTION_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 3",
+//                "Warning",
+//                JOptionPane.YES_NO_OPTION,
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 4",
+//                "Plain",
+//                JOptionPane.OK_CANCEL_OPTION,
+//                JOptionPane.PLAIN_MESSAGE);
+//        
         // VARIABLES:
         
         // simple variables
@@ -140,7 +140,25 @@ public class ComputerScience30S
         // variable always goes on the LHS
         // and the value going in it goes on the RHS
         
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
         
+        // TIP: on the lower toolbar at the end,
+        // are 2 button to comment an uncomment
+        // code you have selected
+        
+        char d = '*';
+        System.out.println("d = " + d);
+        
+        // more "complex" data type
+        String e = "cats";
+        System.out.println("e = " + e);
+        
+        // in java words that start with
+        // a capital letter, are more
+        // "powerful" which means they have
+        // built-in actions (they are a class)
         
         
         
