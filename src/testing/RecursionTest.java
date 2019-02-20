480bd71992efc88bf39cc7005118e2d8cb0e1f39
Mr. Wachs <mrLWachs@gmail.com>
2019-02-20 10:54:24 -0600

    New 2018-2019 School Year

---
+++ src/testing/RecursionTest.java
@@ -0,0 +1,36 @@
+/*
+ *  Mr. Wachs' classes - Java Computer Science learning file RecursionTest.java
+ *  for project ComputerScience40S on 20-Feb-2019 at 10:39:52 AM by lawrence.wachs
+ */
+
+
+/** required package class namespace */
+package testing;
+
+import tools.Calculator;
+
+
+
+/**
+ * RecursionTest.java - tests the concepts learned in this unit
+ *
+ * @author Mr. Wachs 
+ * @since 20-Feb-2019 
+ */
+public class RecursionTest 
+{
+
+    /** 
+     * Default constructor for the class 
+     */
+    public RecursionTest() {
+        System.out.println("\nRecursion unit test started...\n");
+        
+        int answer = Calculator.factorial(5);
+                
+        System.out.println("\nRecursion unit test complete!\n");
+    }
+    
+    
+    
+}
---
+++ src/tools/Calculator.java
@@ -0,0 +1,19 @@
+
+/** required package class namespace */
+package tools;
+
+
+/**
+ * Calculator.java - useful collection of methods for doing calculations.
+ *
+ * @author Mr. Wachs 
+ * @since 20-Feb-2019 
+ */
+public class Calculator 
+{
+    
+    public static int factorial(int number) {
+        return 0;
+    }
+        
+}
