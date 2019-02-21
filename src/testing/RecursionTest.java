619d86725e5b1e98af493dd779917802b9c20e9f
Mr. Wachs <mrLWachs@gmail.com>
2019-02-21 12:12:09 -0600

    New 2018-2019 School Year

--- src/testing/RecursionTest.java
+++ src/testing/RecursionTest.java
@@ -1,15 +1,10 @@
-/*
- *  Mr. Wachs' classes - Java Computer Science learning file RecursionTest.java
- *  for project ComputerScience40S on 20-Feb-2019 at 10:39:52 AM by lawrence.wachs
- */
 
 
 /** required package class namespace */
 package testing;
 
+/** required imports */
 import tools.Calculator;
-
-
 
 /**
  * RecursionTest.java - tests the concepts learned in this unit
@@ -19,18 +14,39 @@ import tools.Calculator;
  */
 public class RecursionTest 
 {
-
+    
     /** 
      * Default constructor for the class 
      */
     public RecursionTest() {
-        System.out.println("\nRecursion unit test started...\n");
-        
-        int answer = Calculator.factorial(5);
+        System.out.println("\nRecursion test starting...\n");
                 
-        System.out.println("\nRecursion unit test complete!\n");
+        // edge testing
+        System.out.println("Recursive factorial...........................");
+        final int MIN_FACORIAL = -1;
+        final int MAX_FACORIAL = 20;
+        for (int i = MIN_FACORIAL; i <= MAX_FACORIAL; i++) {
+            long answer = Calculator.factorial(i);
+            System.out.println(i + "\t factorial = \t" + answer);
+        }
+        
+        System.out.println("Recursive powers...........................");
+        final long MIN_EXPONENT = -10;
+        final long MAX_EXPONENT =  10;
+        final long MIN_BASE     = -10;
+        final long MAX_BASE     =  10;        
+        for (long exponent = MIN_EXPONENT; 
+                  exponent <= MAX_EXPONENT; 
+                  exponent++) {            
+            for (long base = MIN_BASE; base <= MAX_BASE; base++) {   
+                long answer = Calculator.power(base,exponent);
+                System.out.println(base + "\t to the exponent \t" + 
+                        exponent + "\t = " + answer);
+            }
+        }
+                
+        System.out.println("\nRecursion test complete!\n");
     }
-    
     
     
 }
