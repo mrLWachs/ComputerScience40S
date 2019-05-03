df0273a58a281c00ddc23cc4e9b94853c44217cf
Mr. Wachs <mrLWachs@gmail.com>
2019-05-03 09:40:16 -0500

    New 2018-2019 School Year

---
+++ src/computerscience30s/Person.java
@@ -0,0 +1,33 @@
+
+package computerscience30s;
+
+
+public class Person 
+{
+    
+    // properties:
+    
+    String name;
+    int age;
+    boolean isMale;
+    boolean isAlive;
+        
+    // methods:
+
+    void talk() {
+        
+    }
+    
+    void birthday() {
+        
+    }
+    
+    void born() {
+        
+    }
+    
+    void die() {
+        
+    }
+       
+}
