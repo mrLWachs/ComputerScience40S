5bce1fb300fdade3538def407ed20c2658598779
Mr. Wachs <mrLWachs@gmail.com>
2019-05-06 12:43:14 -0500

    New 2018-2019 School Year

--- src/computerscience30s/Person.java
+++ src/computerscience30s/Person.java
@@ -1,31 +1,52 @@
 
+/** required package class namespace */
 package computerscience30s;
 
-
+/**
+ * Person.java - represents a person. This is a class with properties (things 
+ * about the class, adjectives, descriptors) and methods (things the class 
+ * can do, verbs, actions). Classes can be created (instantiated) into 
+ * objects (which are instances of that class).
+ *
+ * @author Mr. Wachs 
+ * @since 03-May-2019 
+ */
 public class Person 
 {
     
-    // properties:
+    // Properties: (things about it, adjectives, descriptors).................
     
     String name;
-    int age;
-    boolean isMale;
-    boolean isAlive;
-        
-    // methods:
-
+    int age;       
+    boolean isMale; 
+    boolean isAlive; 
+    
+    // Methods: (things it can do, verbs, actions)............................
+    
+    /**
+     * A person talks (by outputting information to the screen)
+     */
     void talk() {
         
     }
     
+    /**
+     * A person has a birthday (their age goes up by one)
+     */
     void birthday() {
         
     }
     
+    /**
+     * When a person is born
+     */
     void born() {
         
     }
     
+    /**
+     * When a person dies
+     */
     void die() {
         
     }
