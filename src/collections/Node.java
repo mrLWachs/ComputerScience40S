3456eebf3ac8255d4464187db0cd818fa229bf73
Mr. Wachs <mrLWachs@gmail.com>
2019-05-08 14:39:03 -0500

    New 2018-2019 School Year

--- src/collections/Node.java
+++ src/collections/Node.java
@@ -59,8 +59,8 @@ public class Node <T>
      */
     @Override
     public String toString() {
-        if (data == null) return null;          // invalid data, output null
-        return data.toString();                 // output data in node
+        if (data == null) return "Node " + null;    // invalid data, output null
+        return "Node " + data.toString();           // output data in node
     }
         
     /**
