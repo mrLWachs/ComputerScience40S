0660731193f41b17eaecc49b87dc94621be28add
Mr. Wachs <mrLWachs@gmail.com>
2019-05-13 12:24:03 -0500

    New 2018-2019 School Year

--- src/computerscience30s/Student.java
+++ src/computerscience30s/Student.java
@@ -3,9 +3,11 @@
 package computerscience30s;
 
 
-
 /**
- * Student.java - 
+ * Student.java - represents a student. This class uses inheritance to inherit
+ * the properties and methods from the Person class. This means the Student
+ * class is the child (sub-class) of the parent class Person (super class, 
+ * base class). This is sometimes simplified to a "is a" relationship
  *
  * @author Mr. Wachs 
  * @since 9-May-2019 
@@ -13,8 +15,8 @@ package computerscience30s;
 public class Student 
 {
     
-    private double gpa;
-    private int number;
+    private double gpa;                                 // encapsulated 
+    private int number;                                 // properties
     private String[] courses;
     private int intelligence;
     
