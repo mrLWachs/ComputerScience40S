81929f10195d791a8a789e39ab924cc5374bb659
Mr. Wachs <mrLWachs@gmail.com>
2019-05-30 13:28:50 -0500

    New 2018-2019 School Year

---
+++ src/game/tools/GameObject.java
@@ -0,0 +1,14 @@
+
+/** required package class namespace */
+package game.tools;
+
+
+public class GameObject 
+{
+
+    // code temporarily put in (with the "light bulb") to avoid errors
+    boolean isAlive;
+    Coordinates coordinates;
+
+    
+}
