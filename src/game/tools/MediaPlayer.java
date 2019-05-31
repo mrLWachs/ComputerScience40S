6929f4dee895da19c023274aab7c0444c5e5a953
Mr. Wachs <mrLWachs@gmail.com>
2019-05-31 13:57:58 -0500

    New 2018-2019 School Year

--- src/game/tools/MediaPlayer.java
+++ src/game/tools/MediaPlayer.java
@@ -1,59 +1,95 @@
-/*
- *  Mr. Wachs' classes - Java Computer Science learning file MediaPlayer.java
- *  for project ComputerScience30S on 31-May-2019 at 1:23:26 PM by lawrence.wachs
- */
-
 
 /** required package class namespace */
 package game.tools;
 
 
+/** required imports */
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 
 /**
- * MediaPlayer.java - 
+ * MediaPlayer.java - a collection of useful methods for playing media files
  *
- * @author Mr. Wachs 
- * @since 31-May-2019 
+ * @author Mr. Wachs
+ * @since 14-May-2019 
  */
 public class MediaPlayer 
 {
+    
+    private AudioInputStream audioStream;
+    private Clip             clip;
+
 
     /**
-     * Default constructor for the class, sets class properties
+     * Plays the passed file object WAV file as audio
+     * 
+     * @param file the File object (*.wav) to play
+     * @return the status of what occurred when trying to play the file
      */
-    public MediaPlayer() {
-        
+    public String playWAV(File file) {
+        try {       
+            if (clip != null) {
+                if (clip.isRunning()) return "Sound file already playing";
+            }
+            audioStream = AudioSystem.getAudioInputStream(file);
+            clip        = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            return "playing sound file " + file.getName();
+        }
+        catch(UnsupportedAudioFileException error) {
+            return "Cannot play this file type!";                    
+        }
+        catch(LineUnavailableException error) {
+            return "Cannot play any audio files!";
+        }
+        catch(IOException error) {
+            return "File error";
+        } 
+    }
+
+    /** Stops any playing audio clip */
+    public void stop() {
+        if (clip != null) clip.stop();
     }
 
     /**
-     * String representation of this object
-     *
-     * @return The object represented as a String
+     * Plays the passed filename (relative class path *.wav file) as audio
+     * 
+     * @param filename the relative class path to the file (*.wav) to play
      */
-    @Override
-    public String toString() {
-        return "MediaPlayer " + super.toString();
-    }
-        
-    /**
-     * Determines if two objects are "equal" in this context
-     *
-     * @param object the object to compare to
-     * @return the objects are "equal" (true) or not (false)
-     */
-    @Override
-    public boolean equals(Object object) {
-        return super.equals(object);
-    }
-
-    /**
-     * Creates a duplicate object using new memory
-     *
-     * @return a "clone" of the object using new memory
-     */
-    @Override
-    public MediaPlayer clone() {
-        return this;
+    public String playWAV(String filename) {
+        try {      
+            URL url                      = getClass().getResource(filename); 
+            URI uri                      = url.toURI();
+            File file                    = new File(uri);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            clip                         = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            return "playing sound file";
+        }
+        catch(UnsupportedAudioFileException error) {
+            return "Cannot play this file type!";                    
+        }
+        catch(LineUnavailableException error) {
+            return "Cannot play any audio files!";
+        }
+        catch(IOException error) {
+            return "File error";
+        }         
+        catch (URISyntaxException error) {
+            return "File URI error";
+        }
     }
 
 }
