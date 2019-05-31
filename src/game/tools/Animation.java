b69a2ae380c9d81c32053588d50a84299b88872f
Mr. Wachs <mrLWachs@gmail.com>
2019-05-31 10:08:04 -0500

    New 2018-2019 School Year

--- src/game/tools/Animation.java
+++ src/game/tools/Animation.java
@@ -2,9 +2,30 @@
 /** required package class namespace */
 package game.tools;
 
+import collections.LinkedList;
+import javax.swing.JLabel;
+import javax.swing.Timer;
 
+/**
+ * Animation.java - represents an animation of images (GameImage objects) with
+ * useful methods 
+ *
+ * @author Mr. Wachs 
+ * @since 14-May-2019 
+ */
 public class Animation 
 {
 
+    private JLabel                hitbox;    
+    private LinkedList<GameImage> frames;    
+    private Timer                 timer;
+    private LinkedList<String>    imageFiles;
+    private boolean               shouldLoop;
+    private int                   delay;
+    private int                   fps;
+    private int                   currentFrame;    
+    private int                   lastFrame;
+    
+    
 
 }
