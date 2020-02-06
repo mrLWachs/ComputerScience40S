07210f7ac36db7d7570f1bd97cc74e7dc25b31d3
Mr. Wachs <mrLWachs@gmail.com>
2020-02-06 09:25:41 -0600

    Newest one

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -1,6 +1,29 @@
+// anything following the 2 slashes is a comment, until the end of the line
+
+/*  
+    This is for multiple line comments until it sees a star and a slash.
+    So anything between slash star and start slash is considered a commment.
+*/
+
+// TIPS:
+// (1) Hold Alt + scroll mouse to zoom in and out on code
+// (2) Double click the top of a tab of code to make bigger or smaller
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
 /** required package class namespace - the line below is put in
  * automatically, and connects file to the project */
 package computerscience30s;
+
 
 /**
  * ComputerScience30S.java - the large in class example we continue to work on
@@ -8,18 +31,21 @@ package computerscience30s;
  * any in class requests for code examples that might come up over the course
  *
  * @author Mr. Wachs
- * @since 09-Feb-2019
- * @instructor Mr. Wachs
+ * @since Feb. 5, 2020, 1:50:54 p.m.
  */
-public class ComputerScience30S {   // Curly brackets DO NOT remove
+public class ComputerScience30S {       // Curly brackets DO NOT remove
 
     /**
      * Main method for the project
-     *
+     * 
      * @param args the command line arguments
      */
-    public static void main(String[] args) {        
-
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Hello World");
+        
+        
     }
 
 }
