842c43fe31c7541162fa6b46b5f445da72ef62f2
Mr. Wachs <mrLWachs@gmail.com>
2020-02-07 14:13:47 -0600

    Newest one

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -16,6 +16,10 @@
 // (5) Intellisense (autocomplete) can appear if you press "CTRL + SPACE"
 // (6) The word "null" means "nothing" and can act as a placeholder for code 
 //     you don't actually want
+// (7) To "comment out" some code, select 
+//     the code (with your mouse) and 
+//     click on the two buttons under the 
+//     code tab (at the end)
 
 
 /** 
@@ -47,79 +51,140 @@ public class ComputerScience30S {       // Curly brackets DO NOT remove
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+//        // TODO code application logic here
+//        
+//        ///////////////////////////////////////////////////////////////////////
+//        // JAVA BASIC INPUT AND OUTPUTS
+//        ///////////////////////////////////////////////////////////////////////
+//                
+//        // Basic output
+//        System.out.print("Hello World...");
+//        
+//        // If you use println, it adds a line break at the end
+//        System.out.println("I'm in the world");
+//        
+//        // Additional output tips.... (these are "special" characters):        
+//        System.out.println("Quote \" ");
+//        System.out.println("Make a \t tab");
+//        System.out.println("Slash \\ ");
+//        System.out.println("New \n Line");
+//        
+//        // Make a "dialog" box appear        
+//        JOptionPane.showMessageDialog(null, "Hi");
+//        
+//        // Optional (does not "need" to be in assignments) dialog boxes:
+//        JOptionPane.showMessageDialog(null,
+//                "Information dialog icon is i",
+//                "Information title",
+//                JOptionPane.INFORMATION_MESSAGE);
+//        // You can hit "enter" in some lines of code at certain places to 
+//        // drop the code to the next line and save having too long of a line
+//        
+//        JOptionPane.showMessageDialog(null,
+//                "Question dialog icon is ?",
+//                "Question title",
+//                JOptionPane.QUESTION_MESSAGE);
+//        
+//        JOptionPane.showMessageDialog(null,
+//                "Warning dialog icon is !",
+//                "Warning title",
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showMessageDialog(null,
+//                "Plain dialog icon is !",
+//                "Plain title",
+//                JOptionPane.PLAIN_MESSAGE);
+//        
+//        // dialogs for inputs
+//        
+//        // the simpliest version is the one you have to use        
+//        JOptionPane.showInputDialog("Enter something");
+//        
+//        // some optional versions of input dialogs
+//        JOptionPane.showInputDialog(null,
+//                "Information dialog icon is i",
+//                "Information title",
+//                JOptionPane.INFORMATION_MESSAGE);
+//        
+//        JOptionPane.showInputDialog(null,
+//                "Question dialog icon is ?",
+//                "Question title",
+//                JOptionPane.QUESTION_MESSAGE);
+//        
+//        JOptionPane.showInputDialog(null,
+//                "Warning dialog icon is !",
+//                "Warning title",
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showInputDialog(null,
+//                "Plain dialog icon is !",
+//                "Plain title",
+//                JOptionPane.PLAIN_MESSAGE);
+//        
+//        // NOTE: these examples above are NOT being used 
+//        // properly if used in a assignment, correct way
+//        // comes up later in this code...
+//        
+//        // an optional type of dialog (may not be needed)
+//        
+//        JOptionPane.showConfirmDialog(null, "Confirm");
+//        
+//        // more different versions...
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 1",
+//                "Information",
+//                JOptionPane.DEFAULT_OPTION,
+//                JOptionPane.INFORMATION_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 2",
+//                "Question",
+//                JOptionPane.YES_NO_OPTION,
+//                JOptionPane.QUESTION_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 3",
+//                "Warning",
+//                JOptionPane.OK_CANCEL_OPTION,
+//                JOptionPane.WARNING_MESSAGE);
+//        
+//        JOptionPane.showConfirmDialog(null,
+//                "Example 4",
+//                "Plain",
+//                JOptionPane.YES_NO_CANCEL_OPTION,
+//                JOptionPane.PLAIN_MESSAGE);
+//        
         ///////////////////////////////////////////////////////////////////////
-        // JAVA BASIC INPUT AND OUTPUTS
+        // VARIABLES:
         ///////////////////////////////////////////////////////////////////////
-                
-        // Basic output
-        System.out.print("Hello World...");
         
-        // If you use println, it adds a line break at the end
-        System.out.println("I'm in the world");
+        // the simpliest of variable types
         
-        // Additional output tips.... (these are "special" characters):        
-        System.out.println("Quote \" ");
-        System.out.println("Make a \t tab");
-        System.out.println("Slash \\ ");
-        System.out.println("New \n Line");
+        boolean a = true;
+        // type name = value ;
+        // how to create "declare" a variable 
         
-        // Make a "dialog" box appear        
-        JOptionPane.showMessageDialog(null, "Hi");
+        int b = 0;
         
-        // Optional (does not "need" to be in assignments) dialog boxes:
-        JOptionPane.showMessageDialog(null,
-                "Information dialog icon is i",
-                "Information title",
-                JOptionPane.INFORMATION_MESSAGE);
-        // You can hit "enter" in some lines of code at certain places to 
-        // drop the code to the next line and save having too long of a line
+        double c = 0.0;
         
-        JOptionPane.showMessageDialog(null,
-                "Question dialog icon is ?",
-                "Question title",
-                JOptionPane.QUESTION_MESSAGE);
+        // variables can change (vary)
+        a = false;
+        b = 1;
+        c = 1.2;
         
-        JOptionPane.showMessageDialog(null,
-                "Warning dialog icon is !",
-                "Warning title",
-                JOptionPane.WARNING_MESSAGE);
+        // variable always goes on the 
+        // LHS (left hand side) of the
+        // equals sign (=) and the value
+        // going into the variable always
+        // goes on the RHS (right hand
+        // side)
         
-        JOptionPane.showMessageDialog(null,
-                "Plain dialog icon is !",
-                "Plain title",
-                JOptionPane.PLAIN_MESSAGE);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
         
-        // dialogs for inputs
-        
-        // the simpliest version is the one you have to use        
-        JOptionPane.showInputDialog("Enter something");
-        
-        // some optional versions of input dialogs
-        JOptionPane.showInputDialog(null,
-                "Information dialog icon is i",
-                "Information title",
-                JOptionPane.INFORMATION_MESSAGE);
-        
-        JOptionPane.showInputDialog(null,
-                "Question dialog icon is ?",
-                "Question title",
-                JOptionPane.QUESTION_MESSAGE);
-        
-        JOptionPane.showInputDialog(null,
-                "Warning dialog icon is !",
-                "Warning title",
-                JOptionPane.WARNING_MESSAGE);
-        
-        JOptionPane.showInputDialog(null,
-                "Plain dialog icon is !",
-                "Plain title",
-                JOptionPane.PLAIN_MESSAGE);
-        
-        // NOTE: these examples above are NOT being used 
-        // properly if used in a assignment, correct way
-        // comes up later in this code...
         
         
         
