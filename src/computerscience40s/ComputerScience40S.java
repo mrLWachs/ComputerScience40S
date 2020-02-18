02b7517718d86bb6452d1e3b66efa398778a1142
Mr. Wachs <mrLWachs@gmail.com>
2020-02-18 11:41:39 -0600

    Newest one

--- src/computerscience40s/ComputerScience40S.java
+++ src/computerscience40s/ComputerScience40S.java
@@ -1,3 +1,44 @@
+// NETBEANS TIPS:
+// ==============
+// (1)  Hold Alt + scroll mouse to zoom in and out on code
+// (2)  Double click the top of a tab of code to make bigger or smaller
+// (3)  Type in lowercase "sout" and hit TAB to write System.out....
+// (4)  Words in capital letters and in blue are IMPORTANT code words, the words
+//      in captial letters are powerful, they are a "class"
+// (5)  Intellisense (autocomplete) can appear if you press "CTRL + SPACE"
+// (6)  The word "null" means "nothing" and can act as a placeholder for code 
+//      you don't actually want
+// (7)  To "comment out" some code, select the code (with your mouse) and 
+//      click on the two buttons under the code tab (at the right end)
+// (8)  A red squiggily line and/or a "lightbulb" in NetBeans means a potential 
+//      error and a potential fix (if you click on the lightbulb)
+// (9)  You can get more NetBeans tips and autocompletes by selecting 
+//      Help -> Keyboard Shortcuts Card
+// (10) To stop a running program, click the red stop button on the left in the
+//      output window, or the X in the bottom right of NetBeans
+// (11) Go to Tools -> Templates, scroll down and click on the "+" beside 
+//      "Java", select "Java Main Class", and "Open in Editor" and the enter 
+//      this into the temple:
+//
+//      /**
+//        * ${name}.java - 
+//        *
+//        * @author Mr. Wachs 
+//        * @since ${date} 
+//        */
+//       public class ${name} 
+//       { .... etc .....     
+// 
+// (12) When printing code for hand in (this only needs to be done once), click
+//      on "Print Options" and uncheck everything previously checked off, and
+//      then check off "Print as in Editor", then either print it to a PDF 
+//      (which means select "Microsoft Print to PDF" on the next screen), or
+//      cancel and resize the code to fit better (see tip #1) and then print 
+//      again
+// (13) To autoformat code (fixes indents, extra spaces, etc.) select code with
+//      mouse or keyboard (or it will format all if none is selected) and press
+//      ALT + SHIFT + F  (you should do this before hand in!)
+
 
 /** required package class namespace */
 package computerscience40s;
