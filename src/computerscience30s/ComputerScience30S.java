14dae43488f99e66e69d328ee212548bd7ca0f86
Mr. Wachs <mrLWachs@gmail.com>
2020-02-27 16:03:26 -0600

    Newest one

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -6,54 +6,57 @@
     starting with "slash star" until it sees the closing "star slash"
 */
 
-// NETBEANS TIPS:
-// ==============
-// (1)  Hold Alt + scroll mouse to zoom in and out on code
-// (2)  Double click the top of a tab of code to make bigger or smaller
-// (3)  Type in lowercase "sout" and hit TAB to write System.out....
-// (4)  Words in capital letters and in blue are IMPORTANT code words, the words
-//      in captial letters are powerful, they are a "class"
-// (5)  Intellisense (autocomplete) can appear if you press "CTRL + SPACE"
-// (6)  The word "null" means "nothing" and can act as a placeholder for code 
-//      you don't actually want
-// (7)  To "comment out" some code, select the code (with your mouse) and 
-//      click on the two buttons under the code tab (at the right end)
-// (8)  A red squiggily line and/or a "lightbulb" in NetBeans means a potential 
-//      error and a potential fix (if you click on the lightbulb)
-// (9)  You can get more NetBeans tips and autocompletes by selecting 
-//      Help -> Keyboard Shortcuts Card
-// (10) To stop a running program, click the red stop button on the left in the
-//      output window, or the X in the bottom right of NetBeans
-// (11) Go to Tools -> Templates, scroll down and click on the "+" beside 
-//      "Java", select "Java Main Class", and "Open in Editor" and the enter 
-//      this into the temple:
-//
-//      /**
-//        * ${name}.java - 
-//        *
-//        * @author Mr. Wachs 
-//        * @since ${date} 
-//        */
-//       public class ${name} 
-//       { .... etc .....     
-// 
-// (12) When printing code for hand in (this only needs to be done once), click
-//      on "Print Options" and uncheck everything previously checked off, and
-//      then check off "Print as in Editor", then either print it to a PDF 
-//      (which means select "Microsoft Print to PDF" on the next screen), or
-//      cancel and resize the code to fit better (see tip #1) and then print 
-//      again
-// (13) To autoformat code (fixes indents, extra spaces, etc.) select code with
-//      mouse or keyboard (or it will format all if none is selected) and press
-//      ALT + SHIFT + F  (you should do this before hand in!)
-// (14) When handing in multiple Java class files, step 1: make sure they are 
-//      all open in tabs, Step 2: use the mouse (and the keyboard, holding 
-//      down the "CTRL" key) to select all the class files in the Projects 
-//      window, Step 3: Go to File -> Print
-// (15) You can "teach" NetBeans to change the style it uses to autoformat (see
-//      number 13) by going to Tools -> Options, then click on the "Editor"
-//      tab at the top, then the "Formatting" below that. Then change the 
-//      settings to what you prefer
+/**
+ * NETBEANS TIPS:
+ * ==============
+ * (1)  Hold Alt + scroll mouse to zoom in and out on code
+ * (2)  Double click the top of a tab of code to make bigger or smaller
+ * (3)  Type in lowercase "sout" and hit TAB to write System.out....
+ * (4)  Words in capital letters and in blue are IMPORTANT code words, the words
+ *      in captial letters are powerful, they are a "class"
+ * (5)  Intellisense (autocomplete) can appear if you press "CTRL + SPACE"
+ * (6)  The word "null" means "nothing" and can act as a placeholder for code 
+ *      you don't actually want
+ * (7)  To "comment out" some code, select the code (with your mouse) and 
+ *      click on the two buttons under the code tab (at the right end)
+ * (8)  A red squiggily line and/or a "lightbulb" in NetBeans means a potential 
+ *      error and a potential fix (if you click on the lightbulb)
+ * (9)  You can get more NetBeans tips and autocompletes by selecting 
+ *      Help -> Keyboard Shortcuts Card
+ * (10) To stop a running program, click the red stop button on the left in the
+ *      output window, or the X in the bottom right of NetBeans
+ * (11) Go to Tools -> Templates, scroll down and click on the "+" beside 
+ *      "Java", select "Java Main Class", and "Open in Editor" and the enter 
+ *      this into the temple:
+ *
+ *      
+ *         ${name}.java - 
+ *        
+ *         @author Mr. Wachs 
+ *         @since ${date} 
+ *        
+ *       public class ${name} 
+ *       { .... etc .....     
+ * 
+ * (12) When printing code for hand in (this only needs to be done once), click
+ *      on "Print Options" and uncheck everything previously checked off, and
+ *      then check off "Print as in Editor", then either print it to a PDF 
+ *      (which means select "Microsoft Print to PDF" on the next screen), or
+ *      cancel and resize the code to fit better (see tip #1) and then print 
+ *      again
+ * (13) To autoformat code (fixes indents, extra spaces, etc.) select code with
+ *      mouse or keyboard (or it will format all if none is selected) and press
+ *      ALT + SHIFT + F  (you should do this before hand in!)
+ * (14) When handing in multiple Java class files, step 1: make sure they are 
+ *      all open in tabs, Step 2: use the mouse (and the keyboard, holding 
+ *      down the "CTRL" key) to select all the class files in the Projects 
+ *      window, Step 3: Go to File -> Print
+ * (15) You can "teach" NetBeans to change the style it uses to autoformat (see
+ *      number 13) by going to Tools -> Options, then click on the "Editor"
+ *      tab at the top, then the "Formatting" below that. Then change the 
+ *      settings to what you prefer
+*/
+
 
 /** 
  * required package class namespace - the line below is put in
@@ -63,8 +66,8 @@ package computerscience30s;
 
 /** 
  * Required imports for the project - the lines below are added when your
-  * code needs to connect to another library (API) of code 
-  */
+ * code needs to connect to another library (API) of code 
+ */
 import javax.swing.JOptionPane;
 
 
@@ -84,6 +87,10 @@ public class ComputerScience30S {       // Curly brackets DO NOT remove
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        learnMethods();
+    }
+
+    private static void learnMethods() {
         // TODO code application logic here
         
         ///////////////////////////////////////////////////////////////////////
