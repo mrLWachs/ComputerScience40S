4167fc75362cbf998cf9dc62b1d5ab906df8e20c
Mr. Wachs <mrLWachs@gmail.com>
2020-02-28 14:20:17 -0600

    Newest one

--- src/testing/AdvancedClassesTest.java
+++ src/testing/AdvancedClassesTest.java
@@ -1,5 +1,4 @@
 
-
 /** required package class namespace */
 package testing;
 
@@ -10,7 +9,7 @@ import testing.advancedclasses.Person;
  * AdvancedClassesTest.java - description
  *
  * @author Mr. Wachs
- * @since Feb. 28, 2020, 9:56:32 a.m.
+ * @since Feb. 28, 2020, 2:00:03 p.m.
  */
 public class AdvancedClassesTest 
 {
@@ -19,15 +18,18 @@ public class AdvancedClassesTest
      * Default constructor, set class properties
      */
     public AdvancedClassesTest() {
-        System.out.println("\nAdvanced Classes test started...\n");
+        System.out.println("\nAdvanced classes test started...\n");
         
+        // Review of classes concepts (properties, methods, object, inheritance)
+        
+        // using the class to create an object with the default constructor:
+        // class object = call constructor method (default)
         Person person = new Person();
         
-        person.talk();
         
         
-        System.out.println("\nAdvanced Classes test complete!\n");
+        System.out.println("\nAdvanced classes test complete!\n");
     }
 
-   
+    
 }
--- src/testing/advancedclasses/Person.java
+++ src/testing/advancedclasses/Person.java
@@ -8,29 +8,29 @@ package testing.advancedclasses;
  * Person.java - description
  *
  * @author Mr. Wachs
- * @since Feb. 28, 2020, 9:56:22 a.m.
+ * @since Feb. 28, 2020, 2:02:58 p.m.
  */
 public class Person 
 {
 
     // PROPERTIES (below)...................................................
     
-    
     private String name;    // encapsulated property
     public int age;
     
+    // METHODS (below)......................................................
     
     /**
      * Default constructor, set class properties
      */
     public Person() {
         name = "jane doe";
-        age  = 0;
+        age = 0;
     }
-
+    
     public void talk() {
         System.out.println(name + " is " + age);
     }
-    
 
+    
 }
