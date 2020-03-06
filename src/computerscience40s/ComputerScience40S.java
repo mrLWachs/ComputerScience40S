b0d80289c4ab5fe5dff449bd1e29721e5cd293e2
Mr. Wachs <mrLWachs@gmail.com>
2020-03-06 08:39:00 -0600

    Newest one

--- src/computerscience40s/ComputerScience40S.java
+++ src/computerscience40s/ComputerScience40S.java
@@ -100,4 +100,57 @@ public class ComputerScience40S
 //      methods if you double click them
 // (21) If you type /** before the method signature line it will automate
 //      which JavaDoc comments you should make
+// (22) Go to Tools -> Templates, scroll down and click on the "+" beside 
+//      "Java", select "Java Class", and "Open in Editor" and the enter 
+//      this into the temple:
+//
+//      /**
+//       * ${name}.java - description
+//       *
+//       * @author Mr. Wachs
+//       * @since ${date}, ${time}
+//       */
+//      public class ${name} 
+//      {
+//      
+//          /**
+//           * Default constructor, set class properties
+//           */
+//          public ${name}() {
+//              
+//          }
+//     
+//          /**
+//           * String representation of this object
+//           *
+//           * @return The object represented as a String
+//           */
+//          @Override
+//          public String toString() {
+//              return "${name}: " + super.toString();
+//          }
+//         
+//          /**
+//           * Deep comparison, determines if two objects are "equal" in this context
+//           *
+//           * @param object the object to compare to
+//           * @return the objects are "equal" (true) or not (false)
+//           */
+//          @Override
+//          public boolean equals(Object object) {
+//              return super.equals(object);
+//          }
+//             
+//          /**
+//           * a Deep clone, creates a duplicate object using new memory
+//           *
+//           * @return a "clone" of the object using new memory
+//           */
+//          @Override
+//          public ${name} clone() {
+//              return this;
+//          }
+//     
+//      }
+
 
