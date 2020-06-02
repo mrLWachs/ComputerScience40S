8b8ccb566d6b8cae613177d3637a7d5cdd5015c1
Mr. Wachs <mrLWachs@gmail.com>
2020-06-02 14:09:50 -0500

    Newest one

--- src/testing/RecursionTest.java
+++ src/testing/RecursionTest.java
@@ -1,10 +1,6 @@
 
 /** required package class namespace */
 package testing;
-
-/** required imports */
-import tools.Calculator;
-
  
 /**
  * RecursionTest.java - tests the concepts learned in this unit
@@ -17,30 +13,6 @@ public class RecursionTest
 
     public RecursionTest() {
         System.out.println("\nRecursion test started...\n");
-        
-        // When testing, think of three scenarios:
-            // 1) Typical case
-            // 2) Edge cases
-            // 3) Beyond the edge cases (bad user)
-            
-        System.out.println("Recursive factorial.............................");        
-        final int MIN_FACTORIAL = -1;
-        final int MAX_FACTORIAL = 20;        
-        for (int i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
-            long answer = Calculator.factorial(i);
-            System.out.println(i + "\t factorial = \t" + answer);
-        }
-        
-        System.out.println("Recursive powers................................");        
-        final int MIN_POWER = -1;
-        final int MAX_POWER = 10;        
-        for (int base = MIN_POWER; base <= MAX_POWER; base++) {
-            for (int exponent = MIN_POWER; exponent <= MAX_POWER; exponent++) {
-                double answer = Calculator.power(base,exponent);
-                System.out.println(base + "\t to the exponent \t" +
-                        exponent + "\t = \t" + answer);
-            }
-        }
         
         System.out.println("\nRecursion test complete!\n");
     }
--- src/tools/Calculator.java
+++ src/tools/Calculator.java
@@ -12,44 +12,4 @@ package tools;
 public class Calculator 
 {
 
-    /**
-     * Calculates the factorial on a number using a recursive method. Recursive
-     * methods need at least one base case (to stop the recursion), but can 
-     * have more than one. They also need to have at minimum one recursive 
-     * case (but also can have multiple recursive cases). Recursive cases 
-     * means the method calls itself!
-     * 
-     * @param number the number to calculate the factorial of
-     * @return the factorial of the parameter
-     */
-    public static long factorial(long number) {
-        // base case(s) - stop recursion
-        if (number <= 1) return 1;
-        // recursive case(s) - like a loop, method calls itself
-        return number * factorial(number-1);
-    }
-
-    /**
-     * Calculates the power of a base to it's exponent
-     * 
-     * @param base the base of the power
-     * @param exponent the exponent of the power
-     * @return the base^exponent
-     */
-    public static double power(int base, int exponent) {
-        // base case(s)
-        if (exponent == 0) return 1;
-        if (exponent == 1) return base;
-        // recursive case(s)
-        if (exponent < 0) {                     // for negative exponents
-            if (base != 0) {
-                return 1 / power(base,exponent * -1);
-            }
-            else {                              // cannot divide by zero!
-                return Double.MIN_VALUE;        // special case
-            }
-        }
-        return base * power(base, exponent-1);
-    }
-    
 }
