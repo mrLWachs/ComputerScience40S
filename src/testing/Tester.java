8d5e3cc69938cdcc4d303a141ad99b29376dc281
Mr. Wachs <mrLWachs@gmail.com>
2020-06-02 14:12:39 -0500

    Newest one

--- src/testing/Tester.java
+++ src/testing/Tester.java
@@ -14,9 +14,7 @@ public class Tester
 
     public Tester() {
         System.out.println("\nTesting begins...\n");        
-        new RecursionTest();        // annoymous object
-        new AdvancedClassesTest();
-        new CollectionsTest();        
+        new RecursionTest();        // annoymous object  
         System.out.println("\nTesting complete!\n");
     }
         
