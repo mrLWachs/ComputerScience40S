95964c6d8aed678a8891ee87737ff4b9e133d483
Mr. Wachs <mrLWachs@gmail.com>
2020-06-03 10:28:12 -0500

    Newest one

--- src/testing/advancedclasses/Person.java
+++ src/testing/advancedclasses/Person.java
@@ -20,10 +20,6 @@ public class Person
     private String name;    // encapsulated property
     public int age;
     
-    /** Protected property representing the person's gender */
-    protected boolean isMale;
-    
-    
     // METHODS (below)......................................................
     
     /**
@@ -32,7 +28,6 @@ public class Person
     public Person() {
         name   = "jane doe";
         age    = 0;
-        isMale = false;
     }
     
     /**
@@ -40,102 +35,17 @@ public class Person
      * 
      * @param name the name for this person
      * @param age the age for this person
-     * @param isMale the gender for this person
      */
-    public Person(String name, int age, boolean isMale) {
+    public Person(String name, int age) {
         this.name   = name;
         this.age    = age;
-        this.isMale = isMale;
     }
     
     /**
      * Outputs data on this person to the screen
      */
     public void talk() {
-        System.out.println(this.toString());
-    }
-    
-    /**
-     * String representation of this object
-     *
-     * @return The object represented as a String
-     */
-    @Override
-    public String toString() {
-        return name + "," + age + "," + isMale;
-    }
-    
-    /**
-     * Deep comparison, determines if two objects are "equal" in this context
-     * "shallow" comparison would be: "return true;" and a "deeper" comparison 
-     * would be to first turn (cast) the object into a person and then compare 
-     * ALL the properties of "this" person with ALL the properties of the 
-     * other person object (which we will call "that")
-     * 
-     * @param object the object to compare to
-     * @return the objects are "equal" (true) or not (false)
-     */
-    @Override
-    public boolean equals(Object object) {
-        Person that      = (Person)object; 
-        if (this.age    != that.age)      return false;
-        if (this.isMale != that.isMale)   return false;
-        if (!this.name.equals(that.name)) return false; 
-        return true;
-    }
-    
-    /**
-     * A Deep clone, creates a duplicate object using new memory
-     * A shallow clone would be: "return this;" and a deeper clone is shown
-     * below...
-     *
-     * @return a "clone" of the object using new memory
-     */
-    @Override
-    public Person clone() {
-        Person person = new Person();
-        person.name   = this.name;
-        person.age    = this.age;
-        person.isMale = this.isMale;
-        return person;
-    }
-    
-    /**
-     * Accessor method of the private property
-     * 
-     * @return the property to "see"
-     */
-    public String getName() {
-        return name;
-    }
-    
-    /**
-     * Mutator method to change the private property
-     * 
-     * @param name the property to mutate
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    /**
-     * Eats the passed food by outputting to the screen
-     * 
-     * @param food the abstract food object to eat 
-     */
-    public void haveDinner(Food food) {
-        if (food.nurishment) {
-            if (food.flavour.equals("sweet")) {
-                food.smell();
-            }
-            food.prepare();
-            food.eat();
-            food.digest();
-            food.store();
-        }
-        else {
-            System.out.println("Don't eat it");
-        }        
+        System.out.println(name + "," + age);
     }
         
 }
--- src/testing/advancedclasses/Student.java
+++ src/testing/advancedclasses/Student.java
@@ -16,9 +16,6 @@ public class Student extends Person
 
     private int studentNumber;                      // encapsulated property
     
-    /** Static property that counts the total number of student objects */
-    public static int totalStudents;
-    
     
     /**
      * Class constructor sets class properties, when a constructor has a 
@@ -30,69 +27,16 @@ public class Student extends Person
      */
     public Student(int studentNumber) {
         super();                                // call to super constructor
-        this.studentNumber = studentNumber;     // set property from parameter 
-        super.isMale = true;
-        totalStudents++;
+        this.studentNumber = studentNumber;     // set property from parameter
     }
-    
-    /**
-     * Class constructor sets class properties to parameter values
-     * 
-     * @param name the name for this student
-     * @param age the age for this student
-     * @param isMale the gender for this student
-     * @param studentNumber the student number for this student
-     */
-    public Student(String name, int age, boolean isMale, int studentNumber) {
-        super(name, age, isMale);
-        this.studentNumber = studentNumber;
-        totalStudents++;
-    }
-      
+ 
     /**
      * Outputs data on this student to the screen
      */
     @Override
     public void talk() {
-        System.out.println(this.toString());
-    }
-    
-    /**
-     * String representation of this object
-     *
-     * @return The object represented as a String
-     */
-    @Override
-    public String toString() {
-        return super.toString() + "," + studentNumber;
-    }
-    
-    /**
-     * Deep comparison, determines if two objects are "equal" in this context
-     *
-     * @param object the object to compare to
-     * @return the objects are "equal" (true) or not (false)
-     */
-    @Override
-    public boolean equals(Object object) {
-        Student that = (Student)object;             // cast into student object
-        if (this.studentNumber != that.studentNumber) return false;
-        if (!super.equals(that))                      return false;        
-        return true;                                // passed the tests
-    }
-       
-    /**
-     * a Deep clone, creates a duplicate object using new memory
-     *
-     * @return a "clone" of the object using new memory
-     */
-    @Override
-    public Student clone() {
-        Student that = new Student(this.studentNumber); // construct student
-        that.age     = this.age;                        // copy properties
-        that.isMale  = this.isMale;
-        that.setName(this.getName());                   // accessor/mutator
-        return that;                                    // return new memory
+        super.talk();
+        System.out.println("#" + studentNumber);
     }
     
 }
