743a21fb97c309ca23b8c86e67a2e418264df454
Mr. Wachs <mrLWachs@gmail.com>
2020-10-07 12:34:09 -0500

    Merge origin/Unit1Recursion into Unit0Begin
    Conflicts:
    	src/testing/Tester.java

--- src/testing/RecursionTest.java
+++ src/testing/RecursionTest.java
@@ -1,16 +1,15 @@
 
 /** required package class namespace */
 package testing;
-
+ 
 /** required imports */
 import tools.Calculator;
 
- 
 /**
  * RecursionTest.java - tests the concepts learned in this unit
  *
  * @author Mr. Wachs
- * @since Feb. 18, 2020, 1:42:34 p.m.
+ * @since Oct. 5, 2020
  */
 public class RecursionTest 
 {
@@ -19,26 +18,30 @@ public class RecursionTest
         System.out.println("\nRecursion test started...\n");
         
         // When testing, think of three scenarios:
-            // 1) Typical case
-            // 2) Edge cases
-            // 3) Beyond the edge cases (bad user)
+            // (1) Typical case (user does what they are told)
+            // (2) Edge cases (user pushes the edges)
+            // (3) Beyond the edges (bad users, tying to break things)
             
-        System.out.println("Recursive factorial.............................");        
+        System.out.println("Recursive factorial................");
+            
         final int MIN_FACTORIAL = -1;
-        final int MAX_FACTORIAL = 20;        
-        for (int i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
+        final int MAX_FACTORIAL = 20;
+        
+        for (long i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
             long answer = Calculator.factorial(i);
             System.out.println(i + "\t factorial = \t" + answer);
         }
         
-        System.out.println("Recursive powers................................");        
-        final int MIN_POWER = -1;
-        final int MAX_POWER = 10;        
+        System.out.println("Recursive powers................");
+        
+        final int MAX_POWER = 20;
+        final int MIN_POWER = -MAX_POWER;
+                
         for (int base = MIN_POWER; base <= MAX_POWER; base++) {
-            for (int exponent = MIN_POWER; exponent <= MAX_POWER; exponent++) {
-                double answer = Calculator.power(base,exponent);
-                System.out.println(base + "\t to the exponent \t" +
-                        exponent + "\t = \t" + answer);
+            for (int exponent = MIN_POWER; exponent <= MAX_POWER; exponent++) {                
+                double answer = Calculator.power(base,exponent);                
+                System.out.println("Base \t" + base + "\t Exponent \t" +
+                                   exponent + "\t = \t" + answer);                
             }
         }
         
--- src/tools/Calculator.java
+++ src/tools/Calculator.java
@@ -7,7 +7,7 @@ package tools;
  * Calculator.java - useful collection of methods for doing calculations.
  *
  * @author Mr. Wachs
- * @since Feb. 18, 2020, 1:42:06 p.m.
+ * @since Oct. 5, 2020
  */
 public class Calculator 
 {
@@ -22,13 +22,13 @@ public class Calculator
      * @param number the number to calculate the factorial of
      * @return the factorial of the parameter
      */
-    public static long factorial(long number) {
-        // base case(s) - stop recursion
-        if (number <= 1) return 1;
-        // recursive case(s) - like a loop, method calls itself
-        return number * factorial(number-1);
+    public static long factorial(long number) {         
+        // Base case(s) - stops the recursion        
+        if (number <= 1) return 1;        
+        // Recursive case(s) - like a loop, method calls itself         
+        return number * factorial(number - 1); 
     }
-
+    
     /**
      * Calculates the power of a base to it's exponent
      * 
@@ -37,19 +37,23 @@ public class Calculator
      * @return the base^exponent
      */
     public static double power(int base, int exponent) {
-        // base case(s)
+        // Base case(s):
+        
+        // Special base case for negative exponents and base of zero
+        // we cannot divide by zero, so return a flag value
+        if (exponent < 0 && base == 0) return Double.MIN_VALUE;
+        
+        // Other base cases for exponents
         if (exponent == 0) return 1;
-        if (exponent == 1) return base;
-        // recursive case(s)
-        if (exponent < 0) {                     // for negative exponents
-            if (base != 0) {
-                return 1 / power(base,exponent * -1);
-            }
-            else {                              // cannot divide by zero!
-                return Double.MIN_VALUE;        // special case
-            }
-        }
+        if (exponent == 1) return base; 
+        
+        // Recursive case(s):
+        
+        // Special recursive case for negative exponents
+        if (exponent < 0) return 1 / power(base,exponent * -1);
+        
+        // Default recursive case
         return base * power(base, exponent-1);
     }
-    
+ 
 }
