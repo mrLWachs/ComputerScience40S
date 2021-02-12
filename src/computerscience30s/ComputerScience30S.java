b43d33b5daceca021abc42054eeb370d89daba8b
Mr. Wachs <mrLWachs@gmail.com>
2021-02-12 10:19:21 -0600

    The assignment solutions finished

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -15,7 +15,21 @@ public class ComputerScience30S {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here        
+        // This is a comment all typing after "//" until the line ends        
+        // Commments are not code, so you can write what you want        
+        // Curly brackets (braces, parenthesis) { and } are important        
+        // All code right now goes between these two brackets
+        
+        
+        
     }
     
 }
+
+
+
+// NETBEANS TIPS:
+// (1) To zoom in and out on the code, hold ALT and roll the mouse
+// (2) Double click the top of a tab to hide side windows or show them
+// (3) 
