c137601cb0ad399382e72395e74d0fcf090befa7
Mr. Wachs <mrLWachs@gmail.com>
2021-02-12 12:02:08 -0600

    The assignment solutions finished

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -21,7 +21,7 @@ public class ComputerScience30S {
         // Curly brackets (braces, parenthesis) { and } are important        
         // All code right now goes between these two brackets
         
-        
+        System.out.println("Hello World");
         
     }
     
@@ -32,4 +32,12 @@ public class ComputerScience30S {
 // NETBEANS TIPS:
 // (1) To zoom in and out on the code, hold ALT and roll the mouse
 // (2) Double click the top of a tab to hide side windows or show them
-// (3) 
+// (3) The curly brackets (braces, parenthesis) are important { }
+// (4) Type "sout" and then press tab it fills (autocompletes) a line of code
+// (5) Click the green triangle on the top toolbar to run code
+
+
+
+
+
+
