3bf616083707a4456480903d69923b036cee5255
Mr. Wachs <mrLWachs@gmail.com>
2021-02-17 11:27:13 -0600

    The assignment solutions finished

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -5,6 +5,7 @@
  */
 package computerscience30s;
 
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -34,6 +35,117 @@ public class ComputerScience30S {
                 
         // Basic output
         System.out.println("Hello World...");
+        // Use print without the "ln", does not add a line break at the end
+        System.out.println("I'm in the world");
+        
+        System.out.println("Good-bye world!");
+                
+        // Additional output tips... (these are "special" characters)
+        
+        System.out.println("Make a quote \" ");
+        System.out.println("Make a slash \\ ");
+        System.out.println("Make a \t tab");
+        System.out.println("Make a \n new line");
+        
+        // Make a dialog box appear
+        JOptionPane.showMessageDialog(null, "Hello");
+        
+        // Above is the only one need for assignments, final projects, etc.
+        // Optional (not needed, but cool) dialog boxes below:
+        
+        JOptionPane.showMessageDialog(
+                null,
+                "Information icon is i",
+                "Information Title",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+        
+        JOptionPane.showMessageDialog(
+                null,
+                "Question icon is ?",
+                "Question Title",
+                JOptionPane.QUESTION_MESSAGE
+        );
+        
+        JOptionPane.showMessageDialog(
+                null,
+                "Warning icon is !",
+                "Warning Title",
+                JOptionPane.WARNING_MESSAGE
+        );
+        
+        JOptionPane.showMessageDialog(
+                null,
+                "Plain no no icon",
+                "Plain Title",
+                JOptionPane.PLAIN_MESSAGE
+        );
+        
+        // Dialogs for inputs....
+        
+        // The simpliest version is the one you have to use
+        JOptionPane.showInputDialog("Enter something");
+        
+        // Some optional input dialogs below:
+        JOptionPane.showInputDialog(
+                null, 
+                "Information dialog icon is i", 
+                "Information Title", 
+                JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showInputDialog(
+                null, 
+                "Question dialog icon is ?", 
+                "Question Title", 
+                JOptionPane.QUESTION_MESSAGE);
+        
+        JOptionPane.showInputDialog(
+                null, 
+                "Warning dialog icon is !", 
+                "Warning Title", 
+                JOptionPane.WARNING_MESSAGE);
+        
+        JOptionPane.showInputDialog(
+                null, 
+                "Plain dialog no icon", 
+                "Plain Title", 
+                JOptionPane.PLAIN_MESSAGE);
+        
+        // More different (and optional) dialogs...
+        
+        JOptionPane.showConfirmDialog(null, "Confirm");
+        
+        JOptionPane.showConfirmDialog(
+                null,
+                "Example 1",
+                "Information",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.INFORMATION_MESSAGE);
+        
+        JOptionPane.showConfirmDialog(
+                null,
+                "Example 2",
+                "Question",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+        
+        JOptionPane.showConfirmDialog(
+                null,
+                "Example 3",
+                "Warning",
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE);
+        
+        JOptionPane.showConfirmDialog(
+                null,
+                "Example 4",
+                "Plain",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE);
+        
+        ///////////////////////////////////////////////////////////////////////
+        // VARIABLES:
+        ///////////////////////////////////////////////////////////////////////
         
     }
     
@@ -47,7 +159,11 @@ public class ComputerScience30S {
 // (3) The curly brackets (braces, parenthesis) are important "{" and "}"
 // (4) Type "sout" and then press tab it fills (autocompletes) "System.out..."
 // (5) Click the green triangle on the top toolbar to run code
-
+// (6) Autocomplete (Intellisense) can appear if you press "CTRL + SPACE"
+// (7) The word "null" means nothing and can act as a placeholder for code
+//     you don't actually need (right now)
+// (8) To "comment out" chunks of code, select the code with your mouse or
+//     keyboard, then click on the two button under the code tab
 
 
 
