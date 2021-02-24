3941510f2d5614ffbd65d33b4afa886c48d526bf
Mr. Wachs <mrLWachs@gmail.com>
2021-02-24 11:57:28 -0600

    The assignment solutions finished!

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -5,8 +5,6 @@
  */
 package computerscience30s;
 
-import javax.swing.JOptionPane;
-
 /**
  *
  * @author mr.wachs
@@ -402,6 +400,34 @@ public class ComputerScience30S {
         if (word1.equals(word2)) {
             System.out.println("w1==w2");
         }
+        else if (word1.equalsIgnoreCase(word2)) {
+            System.out.println("w1 kind of == w2");
+        }
+        else {
+            System.out.println("w1!=w2");
+        }
+        
+        // Use symbols to combine logic, using "and" "or" "not"
+        // The symbols are:
+        // "||" means "or"
+        // "&&" means "and"
+        // "!" means "not
+        
+        int age = 17;
+        double iq = 1.0;
+        
+        if (age >= 21 && iq > 1.5) {
+            // with "and" BOTH have to be true
+            System.out.println("Get in");
+        }
+        else if (age >= 18 || iq > 0.85) {
+            // with "or" EITHER can be true
+            System.out.println("Keep trying");
+        }
+        else {
+            System.out.println("Get out");
+        }
+        
         
         
         
