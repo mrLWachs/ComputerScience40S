ade3c0103739571d8b05619cbc1d2d7bde569ada
Mr. Wachs <mrLWachs@gmail.com>
2021-04-05 12:01:37 -0500

    The assignment solutions finished!

---
+++ src/testing/AdvancedClassesTest.java
@@ -0,0 +1,23 @@
+
+/** Required package class namespace */
+package testing;
+
+/**
+ * AdvancedClassesTest.java - tests the concepts learned in this unit
+ *
+ * @author Mr. Wachs
+ * @since April 5, 2021
+ */
+public class AdvancedClassesTest 
+{
+
+    public AdvancedClassesTest() {
+        // Methods that have the same name as the class (including a capital 
+        // letter at the start) and NO return type (not even the word "void")
+        // is called a "constructor" method
+        // 
+        // Constructor method is called when a object of this class is created
+        
+    }
+    
+}
