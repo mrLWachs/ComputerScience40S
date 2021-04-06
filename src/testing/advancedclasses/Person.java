e20030ff1bdf3fd440db90f8d57e6b3fcfaed93c
Mr. Wachs <mrLWachs@gmail.com>
2021-04-06 12:02:59 -0500

    The assignment solutions finished!

--- src/testing/advancedclasses/Person.java
+++ src/testing/advancedclasses/Person.java
@@ -12,41 +12,10 @@ public class Person
 {
 
     /**
-     * Default constructor, set class properties
+     * Constructor method
      */
     public Person() {
         
-    }
-     
-    /**
-     * String representation of this object
-     *
-     * @return The object represented as a String
-     */
-    @Override
-    public String toString() {
-        return "Person: " + super.toString();
-    }
-   
-    /**
-     * Deep comparison, determines if two objects are "equal" in this context
-     *
-     * @param object the object to compare to
-     * @return the objects are "equal" (true) or not (false)
-     */
-    @Override
-    public boolean equals(Object object) {
-        return super.equals(object);
-    }
-       
-    /**
-     * a Deep clone, creates a duplicate object using new memory
-     *
-     * @return a "clone" of the object using new memory
-     */
-    @Override
-    public Person clone() {
-        return this;
     }
     
 }
