d91e6bf4306628f074405cb1d16df9b5daee9654
Mr. Wachs <mrLWachs@gmail.com>
2021-04-12 11:45:52 -0500

    The assignment solutions finished!

--- src/testing/advancedclasses/Student.java
+++ src/testing/advancedclasses/Student.java
@@ -1,7 +1,21 @@
 
 package testing.advancedclasses;
 
-public class Student 
+
+/**
+ * Student.java - The line below is called the "signature" line for this class.
+ * If we add the "modifier" word "extends" it causes inheritance and then the 
+ * class it inherits from is typed next. This is a "is a" relationship, so a 
+ * "Student is a Person", and this class inherits all properties and methods 
+ * from the "parent" (super) class.
+ * 
+ * @author Mr. Wachs
+ * @since April 2021
+ */
+public class Student extends Person
 {
+
+    
+    
     
 }
