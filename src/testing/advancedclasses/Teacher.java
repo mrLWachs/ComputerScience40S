ebf6d8b3143ab8789e162435fea8ad0ce71d3095
Mr. Wachs <mrLWachs@gmail.com>
2021-05-05 12:09:32 -0500

    The assignment solutions finished!

--- src/testing/advancedclasses/Teacher.java
+++ src/testing/advancedclasses/Teacher.java
@@ -47,23 +47,22 @@ public class Teacher extends Person
     public String toString() {
         // Create a temporary variable to hold all the output data
         String text = "Teacher: " + super.toString();
-        // Now add all the student data (from the array) to this variable
-        // by looping through the entire array (start to finish) - using a 
-        // NEW type of loop called the "enhanced for loop". to create the 
-        // enhanced for loop use the shortcut: type "for" then CTRL + SPACE
-        // and pick the fourth option from the top...        
+        // Now add all the student data (from the array) to this variable by
+        // looping through the entire array (start to finish) - using a NEW type
+        // of loop called the "enhanced for loop". to create the enhanced for
+        // loop use the shortcut: type "for" then CTRL + SPACE and pick the 
+        // fourth option from the top...        
         for (Student student : students) {
             // The enhanced for loop should be read as:
             // "for every student in students..."
-            // the "student" (no "s") object automatically goes through each
-            // element in the array, automatically from the start of the array
-            // to the end, changing "student" each time through to each element
-            // in order...
+            // The "student" object automatically goes through each element in
+            // the array, automatically from the start of the array to the end,
+            // changing "student" each time through to each element in order...
             if (student != null) {  // only count student objects in the array
                 text = text + "\n\t" + student.toString(); // attach to variable
             }
         }        
-        return text;
+        return text;        // Send back the temporary variable
     }
    
     /**
