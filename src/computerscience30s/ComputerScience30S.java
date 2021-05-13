0d8ec9119ace243d43f4553d0c63e5c1df8693e0
Mr. Wachs <mrLWachs@gmail.com>
2021-05-13 11:32:56 -0500

    The assignment solutions finished!

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -851,11 +851,11 @@ public class ComputerScience30S {
         // Arrays can be INPUT(s) or OUTPUT from a method
         
         output(array);    // Send an array to the method as a parameter
+        output(numbers);  // Call the method with a different argument
+        output(coins);    // Overload the "output" method again
         
-        
-        
-        
-        
+        int[] randoms= random(1,10,50);    // Overload "random" method
+        output(randoms);
         
         
         
@@ -881,6 +881,47 @@ public class ComputerScience30S {
         output(text);                               // Output that string
     }
 
+    /**
+     * Convert all the doubles into integers and then output the array in a 
+     * dialog
+     * 
+     * @param array the array of doubles
+     */
+    private static void output(double[] array) {
+        int[] a = convert(array);   // Create int array of converted doubles        
+        output(a);                  // Now output the converted array
+    }
+
+    /**
+     * Converts an array of doubles into an array of integers
+     * 
+     * @param array the array of doubles
+     * @return an array of integers
+     */
+    private static int[] convert(double[] array) {
+        // Methods can return (output) entire arrays
+        int[] a = new int[array.length];      // Create empty array (same size)
+        for (int i = 0; i < a.length; i++) {  // Traverse array
+            a[i] = (int)array[i];             // Cast the double into a int
+        }
+        return a;                             // Return the converted new array
+    }
+
+    /**
+     * Generates an array of random integers in the range between low and high
+     * 
+     * @param low the lowest number in the range
+     * @param high the highest number in the range
+     * @param size the size to make the array
+     * @return an array of random integers
+     */
+    private static int[] random(int low, int high, int size) {
+        int[] numbers = new int[size];      // Create empty array of passed size
+        for (int i = 0; i < size; i++) {    // Traverse array 
+            numbers[i] = random(low, high); // Assign random value to each index
+        }
+        return numbers;                     // Return completed array
+    }
     
 }
 
