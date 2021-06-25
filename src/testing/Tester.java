a438bebbfb0b21973626bc144cd39cbfcf0735df
Mr. Wachs <mrLWachs@gmail.com>
2021-06-25 14:22:34 -0500

    Update Tester.java

--- src/testing/Tester.java
+++ src/testing/Tester.java
@@ -1,5 +1,5 @@
 
-/** required package class namespace */
+/** Required package class namespace */
 package testing;
 
  
@@ -7,14 +7,17 @@ package testing;
  * Tester.java - tests the concepts learned in all the units.
  *
  * @author Mr. Wachs
- * @since Feb. 18, 2020, 1:42:24 p.m.
+ * @since March 4, 2021
  */
 public class Tester 
 {
 
+    /**
+     * Default constructor, sets class properties
+     */
     public Tester() {
-        System.out.println("\nTesting begins...\n");        
-        new RecursionTest();        // annoymous object     
+        System.out.println("\nTesting begins...\n");  
+        
         System.out.println("\nTesting complete!\n");
     }
         
