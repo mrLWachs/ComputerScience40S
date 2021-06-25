e3cb978e52c0b75da66f0f521a23426e1bc38603
Mr. Wachs <mrLWachs@gmail.com>
2021-06-25 14:30:50 -0500

    Update RecursionTest.java

--- src/testing/RecursionTest.java
+++ src/testing/RecursionTest.java
@@ -1,47 +1,49 @@
 
-/** required package class namespace */
+/** Required package class namespace */
 package testing;
- 
-/** required imports */
+
+/** Required imports */
 import tools.Calculator;
 
+ 
 /**
  * RecursionTest.java - tests the concepts learned in this unit
  *
  * @author Mr. Wachs
- * @since Oct. 5, 2020
+ * @since March 4, 2021
  */
 public class RecursionTest 
 {
 
+    /**
+     * Default constructor, sets class properties
+     */
     public RecursionTest() {
         System.out.println("\nRecursion test started...\n");
         
         // When testing, think of three scenarios:
-            // (1) Typical case (user does what they are told)
-            // (2) Edge cases (user pushes the edges)
-            // (3) Beyond the edges (bad users, tying to break things)
+            // 1) Typical case: the user does what they are told
+            // 2) Edge cases: the user pushes the "edges"
+            // 3) Beyond the edges: bad users, tying to break things
             
-        System.out.println("Recursive factorial................");
-            
-        final int MIN_FACTORIAL = -1;
-        final int MAX_FACTORIAL = 20;
+        System.out.println("Recursive factorial............................."); 
         
-        for (long i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
+        final int MIN_FACTORIAL = -1;
+        final int MAX_FACTORIAL = 20;        
+        for (int i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
             long answer = Calculator.factorial(i);
             System.out.println(i + "\t factorial = \t" + answer);
         }
         
-        System.out.println("Recursive powers................");
+        System.out.println("Recursive Powers.................................");
         
-        final int MAX_POWER = 20;
-        final int MIN_POWER = -MAX_POWER;
-                
-        for (int base = MIN_POWER; base <= MAX_POWER; base++) {
-            for (int exponent = MIN_POWER; exponent <= MAX_POWER; exponent++) {                
+        final int MIN_POWER = -10;
+        final int MAX_POWER = 10;                
+        for (int exponent = MIN_POWER; exponent <= MAX_POWER; exponent++) {
+            for (int base = MIN_POWER; base <= MAX_POWER; base++) {                
                 double answer = Calculator.power(base,exponent);                
-                System.out.println("Base \t" + base + "\t Exponent \t" +
-                                   exponent + "\t = \t" + answer);                
+                System.out.println(base + " to the exponent " +
+                                   exponent + " = " + answer);                
             }
         }
         
