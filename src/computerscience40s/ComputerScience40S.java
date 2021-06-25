f30dcf09f7448b283000b2c072aeb4e24d907c22
Mr. Wachs <mrLWachs@gmail.com>
2021-06-25 14:16:26 -0500

    Update ComputerScience40S.java

--- src/computerscience40s/ComputerScience40S.java
+++ src/computerscience40s/ComputerScience40S.java
@@ -1,8 +1,8 @@
 
-/** required package class namespace */
+/** Required package class namespace */
 package computerscience40s;
 
-/** required imports */
+/** Required imports */
 import testing.Tester;
 
 
@@ -13,7 +13,7 @@ import testing.Tester;
  * over the course
  *
  * @author Mr. Wachs 
- * @since 7-Sep-2018 
+ * @since March 4, 2021
  */
 public class ComputerScience40S 
 {
@@ -34,54 +34,50 @@ public class ComputerScience40S
 
 
 
-
+// <editor-fold>   
 
 // NETBEANS TIPS:
 // ==============
-// (1)  Hold Alt + scroll mouse to zoom in and out on code
-// (2)  Double click the top of a tab of code to make bigger or smaller
-// (3)  Type in lowercase "sout" and hit TAB to write System.out....
-// (4)  Words in capital letters and in blue are IMPORTANT code words, the words
-//      in captial letters are powerful, they are a "class"
-// (5)  Intellisense (autocomplete) can appear if you press "CTRL + SPACE"
-// (6)  The word "null" means "nothing" and can act as a placeholder for code 
-//      you don't actually want
-// (7)  To "comment out" some code, select the code (with your mouse) and 
-//      click on the two buttons under the code tab (at the right end)
-// (8)  A red squiggily line and/or a "lightbulb" in NetBeans means a potential 
-//      error and a potential fix (if you click on the lightbulb)
-// (9)  You can get more NetBeans tips and autocompletes by selecting 
-//      Help -> Keyboard Shortcuts Card
-// (10) To stop a running program, click the red stop button on the left in the
+// (1)  To zoom in and out on the code, hold ALT and roll the mouse
+// (2)  Double click the top of a tab to hide side windows or show them
+// (3)  The curly brackets (braces, parenthesis) are important "{" and "}"
+// (4)  Type "sout" and then press tab it fills (autocompletes) "System.out..."
+// (5)  Click the green triangle on the top toolbar to run code
+// (6)  Autocomplete (Intellisense) can appear if you press "CTRL + SPACE"
+// (7)  The word "null" means nothing and can act as a placeholder for code
+//      you don't actually need (right now)
+// (8)  To "comment out" chunks of code, select the code with your mouse or
+//      keyboard, then click on the two buttons under the code tab
+// (9)  To stop a running program, click the red stop button on the left in the
 //      output window, or the X in the bottom right of NetBeans
-// (11) Go to Tools -> Templates, scroll down and click on the "+" beside 
+// (10) You can get more NetBeans tips and autocompletes by selecting 
+//      Help -> Keyboard Shortcuts Card
+// (11) A red squiggily line and/or a "lightbulb" in NetBeans means a potential 
+//      error and a potential fix (if you click on the lightbulb)
+// (12) Go to Tools -> Templates, scroll down and click on the "+" beside 
 //      "Java", select "Java Main Class", and "Open in Editor" and the enter 
-//      this into the temple:
+//      this into the temple (but use the slash "/" instead of "|"):
 //
-//      /**
-//        * ${name}.java - 
-//        *
-//        * @author Mr. Wachs 
-//        * @since ${date} 
-//        */
-//       public class ${name} 
+//      /** 
+//       * ${name}.java - 
+//       *
+//       * @author Mr. Wachs 
+//       * @since ${date} 
+//       */
+//      public class ${name} 
 //       { .... etc .....     
-// 
-// (12) When printing code for hand in (this only needs to be done once), click
+//
+// (13) When printing code for hand in (this only needs to be done once), click
 //      on "Print Options" and uncheck everything previously checked off, and
 //      then check off "Print as in Editor", then either print it to a PDF 
 //      (which means select "Microsoft Print to PDF" on the next screen), or
 //      cancel and resize the code to fit better (see tip #1) and then print 
 //      again
-// (13) To autoformat code (fixes indents, extra spaces, etc.) select code with
+// (14) To autoformat code (fixes indents, extra spaces, etc.) select code with
 //      mouse or keyboard (or it will format all if none is selected) and press
 //      ALT + SHIFT + F  (you should do this before hand in!)
-// (14) When handing in multiple Java class files, step 1: make sure they are 
-//      all open in tabs, Step 2: use the mouse (and the keyboard, holding 
-//      down the "CTRL" key) to select all the class files in the Projects 
-//      window, Step 3: Go to File -> Print
 // (15) You can "teach" NetBeans to change the style it uses to autoformat (see
-//      number 13) by going to Tools -> Options, then click on the "Editor"
+//      number 14) by going to Tools -> Options, then click on the "Editor"
 //      tab at the top, then the "Formatting" below that. Then change the 
 //      settings to what you prefer
 // (16) If you have to rename a variable or a method, select the
@@ -91,7 +87,7 @@ public class ComputerScience40S
 //      press CTRL and SHIFT and "-" it collapses all methods, and
 //      CTRL and SHIFT and "+" expands all code folds
 // (18) If you write the "call" to a method, before writing the method 
-//      itself, the lightbulb (see tip 8) will help you write it
+//      itself, the lightbulb (see tip 11) will help you write it
 // (19) If you hold down the CTRL key, methods calls become links with the 
 //      mouse to the method (even ones we don't write)
 // (20) On the left side window, the Navigator window helps find and jump to
@@ -102,14 +98,11 @@ public class ComputerScience40S
 //      lightbulb to create a method, go to Tools -> Templates, scroll down 
 //      and click on the "+" beside "Java", and then the "+" beside "code
 //      snippets", and select "Generated Method body" and remove the last line
-// (23) Go to Tools -> Templates, scroll down and click on the "+" beside 
-//      "Java", select "Java Class", and "Open in Editor" and the enter 
-//      all the code below into the template (but uncomment it first using the 
-//      button from tip 7) to replace all the code already in the template
-//      then change the @author to your name. NOTE: This can also be used (the 
-//      first part of this) in the interface template as well
+// (23) To modify the template of all new classes, Tools -> Templates, scroll 
+//      down and click on the "+" beside "Java", select "Java Class" and use 
+//      ALL the code (uncommented) below:
 //
-///** required package class namespace */
+///** Required package class namespace */
 //<#if package?? && package != "">
 //package ${package};
 //
@@ -163,4 +156,5 @@ public class ComputerScience40S
 //    }
 //    
 //}
-//
+
+// </editor-fold>
