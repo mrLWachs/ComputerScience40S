314ecc3b6c7bc1b164551e6f9e2238080bd3abb6
Mr. Wachs <mrLWachs@gmail.com>
2022-03-11 15:30:04 -0600

    Sample GitHub Project

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -27,6 +27,10 @@ public class ComputerScience30S {        // Curly brackets DO NOT remove
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        learnIntroToJava();
+    }
+
+    private static void learnIntroToJava() {
         // TODO code application logic here
         
         ///////////////////////////////////////////////////////////////////////
