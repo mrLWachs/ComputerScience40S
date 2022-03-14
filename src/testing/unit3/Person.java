c560379e3e1f5b9709b2b3e2742fda975aaf01dd
Mr. Wachs <mrLWachs@gmail.com>
2022-03-14 15:25:17 -0500

    Sample GitHub Project

---
+++ src/testing/unit3/Person.java
@@ -0,0 +1,20 @@
+
+/** Required package class namespace */
+package testing.unit3;
+
+ 
+/**
+ * Person.java - represents a person, remember that classes are composed of
+ * properties (things about it, descriptors, adjectives) and methods (things
+ * it can do, actions, verbs) and we instantiate (create an instance of) the
+ * class to create an object
+ *
+ * @author Mr. Wachs
+ * @since 14-Mar-2022, 3:13:18 PM
+ */
+public class Person 
+{
+    
+    
+    
+}
