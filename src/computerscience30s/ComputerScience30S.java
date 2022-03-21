80b335e46bb6d7f519091d82375cae195b9d4160
Mr. Wachs <mrLWachs@gmail.com>
2022-03-21 14:58:31 -0500

    Sample GitHub Project

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -702,7 +702,12 @@ public class ComputerScience30S {        // Curly brackets DO NOT remove
         
         write("Number is " + number);
         
-                
+        // Overloaded methods are methods with the same name, BUT different
+        // parameters / arguments
+        
+        output("Hello");
+        
+        output("Good-bye", "Learning Methods");
         
         System.out.println("Learning Methods complete!");
     }
@@ -767,6 +772,28 @@ public class ComputerScience30S {        // Curly brackets DO NOT remove
         double H      = (double)high;
         double number = (H - L + 1) * seed + L;
         return (int)number;
+    }
+    
+    /**
+     * Displays text in a dialog
+     * 
+     * @param text the text to display
+     */
+    private static void output(String text) {
+        JOptionPane.showMessageDialog(null, text);
+    }
+
+    /**
+     * Display text in a dialog with a title at the top of the dialog
+     * 
+     * @param text the text to display
+     * @param title what to show at the top of the dialog
+     */
+    private static void output(String text, String title) {
+        // Overloaded methods are methods with the same name, BUT different
+        // parameters / arguments so we know which version of the method to call
+        JOptionPane.showMessageDialog(null, text, title, 
+                JOptionPane.PLAIN_MESSAGE);
     }
         
 }
