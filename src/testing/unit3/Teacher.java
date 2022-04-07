1a9636b72c70534d2973920c529d8cca8657e656
Mr. Wachs <mrLWachs@gmail.com>
2022-04-07 15:02:52 -0500

    Sample GitHub Project

--- src/testing/unit3/Teacher.java
+++ src/testing/unit3/Teacher.java
@@ -26,14 +26,34 @@ public class Teacher extends Person
      */
     public Student[] students;
     
+    
     /**
      * Constructor for the class sets class properties
      * 
      * @param name the name for this Teacher
      */
     public Teacher(String name) {
+        // Set (or construct) the properties of the parent class
+        
+        // Set the properties fo this class (not the parent class)
+        count = 0;          // Set the "variable" (global) property
+        MAX   = 200;        // Set the constant to a locked value
+        // When declaring / creating (intitializing) an array you use the
+        // keyword "new" and you use the square brackets "[ ]" and the size
+        // of the array is stated inside those brackets when it is declared
+        // This locks the array to that size (that is the maximum number of
+        // elements in that array). The array is indexed so each "spot" (or
+        // index) has an index number that starts at index 0 and goes up to
+        // the length - 1 as the last index        
+        students = new Student[MAX];        
+    }
+    
+    public void add(Student student) {
         
     }
     
-    
+    public void teach() {
+        
+    }    
+        
 }
