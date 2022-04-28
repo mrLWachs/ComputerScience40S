899557be6cc5b08e0e502b903e0074593990bd92
Mr. Wachs <mrLWachs@gmail.com>
2022-04-28 15:28:06 -0500

    Sample GitHub Project

--- src/testing/unit3/Person.java
+++ src/testing/unit3/Person.java
@@ -189,5 +189,30 @@ public class Person
     protected void secret() {
         System.out.println("Shhhh, you shouldn't know about me!");
     }
-        
+    
+    /**
+     * Accessor method (or "getter") that gives you (or "reads") the age of
+     * this person (which is a private/encapsulated property of this class 
+     * which cannot be read outside the class)
+     * 
+     * @return 
+     */
+    public int getAge() {
+        // Often programmers will write a accessor method and NOT a mutator
+        // method for a property - making the property "read-only"
+        return age;
+    }
+    
+    /**
+     * Mutator method (or "setter") that "changes" the encapsulated property
+     * of this person (which is private property/global variable of this class
+     * which cannot be change outside the class)
+     * 
+     * @param age the new age to set (mutate) the property into
+     */
+    public void setAge(int age) {
+        // A programmer "could" potentially write error checkking code here...
+        this.age = age;
+    }    
+    
 }
