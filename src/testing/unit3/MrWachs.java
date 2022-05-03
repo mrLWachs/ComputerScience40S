dab1033c213acd7a8578a2c14d7f644edc4dd83e
Mr. Wachs <mrLWachs@gmail.com>
2022-05-03 14:37:09 -0500

    Sample GitHub Project

--- src/testing/unit3/MrWachs.java
+++ src/testing/unit3/MrWachs.java
@@ -1,21 +1,52 @@
-
 /** Required package class namespace */
 package testing.unit3;
 
  
 /**
- * MrWachs.java - represents a MrWachs Teacher Person
+ * MrWachs.java - description
  *
  * @author Mr. Wachs
- * @since 6-Apr-2022, 3:12:38 PM
+ * @since 3-May-2022, 2:28:39 PM
  */
 public class MrWachs extends Teacher
 {
-    
-    public MrWachs(String name) {
-        super(name);
+
+    /**
+     * Default constructor, set class properties
+     */
+    public MrWachs() {
+        super("Mr. Wachs");
     }
-    
-    
+     
+    /**
+     * String representation of this object
+     *
+     * @return The object represented as a String
+     */
+    @Override
+    public String toString() {
+        return "MrWachs: " + super.toString();
+    }
+   
+    /**
+     * Deep comparison, determines if two objects are "equal" in this context
+     *
+     * @param object the object to compare to
+     * @return the objects are "equal" (true) or not (false)
+     */
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+       
+    /**
+     * a Deep clone, creates a duplicate object using new memory
+     *
+     * @return a "clone" of the object using new memory
+     */
+    @Override
+    public MrWachs clone() {
+        return this;
+    }
     
 }
