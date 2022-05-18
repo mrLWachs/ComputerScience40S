3d8ef680116013d9b8109df7888bd9f83febd18e
Mr. Wachs <mrLWachs@gmail.com>
2022-05-18 15:35:09 -0500

    Sample GitHub Project

--- src/testing/unit3/Apple.java
+++ src/testing/unit3/Apple.java
@@ -3,7 +3,12 @@ package testing.unit3;
 
  
 /**
- * Apple.java - represents a apple
+ * Apple.java - represents an apple. This class inherits from the abstract 
+ * class all its properties and methods. However, the abstract methods are 
+ * FORCED to be over-ridden in this class (now we move from the 'abstract' to
+ * the 'concrete') and the methods MUST now be given a method body (now you
+ * define HOW for this particular food object). Regular methods can still be
+ * over-ridden as well.
  *
  * @author Mr. Wachs
  * @since 17-May-2022, 2:31:36 PM
@@ -15,7 +20,7 @@ public class Apple extends Food
      * Default constructor, set class properties
      */
     public Apple() {
-        super(3);
+        super(3);                   // Call super-constructor, passing the group
     }
        
 }
--- src/testing/unit3/Food.java
+++ src/testing/unit3/Food.java
@@ -56,13 +56,34 @@ public abstract class Food
         this.group = group;     // Assign parameter to private property
     }
     
+    /**
+     * Prepares food to be eaten. By adding the modifier "abstract" to a method,
+     * it makes an abstract method - means the method has no code for the 
+     * body (no curly brackets) and ends in a semi-colon (it can have
+     * parameters and it can return a value)
+     */
     public abstract void prepare();
     
+    /**
+     * Determines if this 'food' item has spoiled
+     * 
+     * @return the food has spoiled (true) or not (false)
+     */
     public abstract boolean hasSpoiled();
     
+    /**
+     * Smell the 'food' item
+     * 
+     * @param seconds how many seconds to smell it
+     */
     public abstract void smell(int seconds);
     
-    
+    /**
+     * How to eat this food object - note: abstract classes can ALSO have
+     * "regular" methods and regular properties (not just abstract methods)
+     * 
+     * @return The output of how eating this "food" item went
+     */
     public String eat() {
         if (hasSpoiled()) {
             return "Do not eat!";
