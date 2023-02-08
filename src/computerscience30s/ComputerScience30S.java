cceab761a7fd392269ffca88b65efd0c4efd349b
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-08 14:15:21 -0600

    Newest

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -8,8 +8,13 @@ public class ComputerScience30S
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // Loquacious
         
     }
 
 }
+
+
+// NETBEANS TIPS:
+// ==============
+// (1)  To zoom in and out on the code, hold ALT and roll the mouse
+// (2)  Double click the top of a tab to hide side windows or show them
