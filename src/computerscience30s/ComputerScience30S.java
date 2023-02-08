01603d04b0cd2eab7b675ccba7fbedbf51443109
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-08 14:11:55 -0600

    Newest

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -2,11 +2,13 @@
 package computerscience30s;
 
 
-public class ComputerScience30S 
-{      
-        
+public class ComputerScience30S
+{
+
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Loquacious
         
     }
 
