eb86f5e6b31f71b8d689653e91f321adec689d23
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-09 13:31:03 -0600

    Newest

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -8,6 +8,18 @@ public class ComputerScience30S
     public static void main(String[] args) {
         // TODO code application logic here
         
+        // This is a comment all typing after "//" until the line ends        
+        // Commments are not code, so you can write what you want        
+        // Curly brackets (braces, parenthesis) { and } are important        
+        // All code right now goes between these two brackets. Comments are 
+        // not code, so you can write what you want
+        
+        
+        
+        
+        
+        
+        
         
     }
 
@@ -18,6 +30,4 @@ public class ComputerScience30S
 // ==============
 // (1)  To zoom in and out on the code, hold ALT and roll the mouse
 // (2)  Double click the top of a tab to hide side windows or show them
-
-
 
