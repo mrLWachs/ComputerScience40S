f060dcb24279344aa75afe389091c1a1f887c0a8
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-09 10:44:56 -0600

    Newest

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -1,6 +1,8 @@
 
 package computerscience30s;
 
+import javax.swing.JOptionPane;
+
 
 public class ComputerScience30S
 {
@@ -41,11 +43,20 @@ public class ComputerScience30S
         // In all these, the key is to use the "\" backslash
         // which is called an "escape character"
         
+        // Make a "fancier" output with a dialog box
         
+        JOptionPane.showMessageDialog(null, "Hi");
         
+        // Above line of code is the ONLY one needed for assignment, final 
+        // project, etc.
         
+        // However, these OPTIONS (not needed, but cool) for other
+        // dialog output message boxes below...
         
-        
+        // Code could span multiple lines until it reaches a semi colon ";"
+        // The light red line on the far right -------------------------------->
+        // can be thought of as a "barrier" and you should try not to code too
+        // much past this line
         
         
         
@@ -71,5 +82,7 @@ public class ComputerScience30S
 // (3)  The curly brackets (braces, parenthesis) are important "{" and "}"
 // (4)  Type "sout" and then press tab it fills (autocompletes) "System.out..."
 // (5)  Click the green triangle on the top toolbar to run code
+// (6)  Autocomplete (Intellisense) can appear if you press "CTRL + SPACE"
+
 
 
