64fcef1e18291afc8b1f388e3a64ffb12810aa90
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-02-14 14:02:48 -0600

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -386,31 +386,39 @@ public class ComputerScience30S
 //        System.out.println("Word " + word + " in upper case is " + upper);
 //        
 //        // Many more string functions to explore (could use CTRL+SPACE)
-        
-        // Another useful "library" of code is the "Math" library which is
-        // also "built-in" (you can easily use it) to Java. It comes with 
-        // its own functions (actions, methods)
-        
-        double answer1 = Math.PI;
-        double answer2 = Math.cos(0.5);
-        double answer3 = Math.abs(-1);
-        double answer4 = Math.pow(5,3);
-        double answer5 = Math.sqrt(9);
-        double answer6 = Math.log(0.5);
-                
-        System.out.println("answer1 = " + answer1);
-        System.out.println("answer2 = " + answer2);
-        System.out.println("answer3 = " + answer3);
-        System.out.println("answer4 = " + answer4);
-        System.out.println("answer5 = " + answer5);
-        System.out.println("answer6 = " + answer6);
+//        
+//        // Another useful "library" of code is the "Math" library which is
+//        // also "built-in" (you can easily use it) to Java. It comes with 
+//        // its own functions (actions, methods)
+//        
+//        double answer1 = Math.PI;
+//        double answer2 = Math.cos(0.5);
+//        double answer3 = Math.abs(-1);
+//        double answer4 = Math.pow(5,3);
+//        double answer5 = Math.sqrt(9);
+//        double answer6 = Math.log(0.5);
+//                
+//        System.out.println("answer1 = " + answer1);
+//        System.out.println("answer2 = " + answer2);
+//        System.out.println("answer3 = " + answer3);
+//        System.out.println("answer4 = " + answer4);
+//        System.out.println("answer5 = " + answer5);
+//        System.out.println("answer6 = " + answer6);
         
         // Math library also includes the ability to make random numbers, like:
         // Random random = new Random();
         
+        double seed = Math.random();
         
+        System.out.println("seed = " + seed);
         
+        // The "seed" is the starting point - generates a random number between
+        // zero and one (never gets to zero or one)
         
+        // You can now use a formula for generating random numbers in a range
+        // between a low and high value (e.g between 1 and 10 and an integer)
+        
+        // Start off with some constants to define the LOW and HIGH
         
         
         
