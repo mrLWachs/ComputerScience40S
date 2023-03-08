7b54b09974dda5e7559583d163a49ddce9274bc6
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-03-08 15:16:11 -0600

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/testing/AdvancedClassesTest.java
+++ src/testing/AdvancedClassesTest.java
@@ -1,10 +1,25 @@
 
+/** Required package class namespace */
 package testing;
 
-
+/** 
+ * AdvancedClassesTest.java - tests the concepts learned in this unit.
+ *
+ * @author Mr. Wachs 
+ * @since March 2023
+ */
 public class AdvancedClassesTest 
 {
 
+    /**
+     * Default constructor method, it sets class properties.
+     * 
+     * Special Methods that have the same name as the class (including a
+     * capital at the start of the name) and NO return type (not even "void")
+     * are called "constructor" methods these methods are called when the class
+     * is created (or constructed), it makes an object from the class, or it
+     * instantiates the class (creates an "instance")
+     */
     public AdvancedClassesTest() {
         System.out.println("\nAdvanced Classes Test started...\n");
         
--- src/testing/classes/Person.java
+++ src/testing/classes/Person.java
@@ -1,7 +1,17 @@
 
+/** Required package class namespace */
 package testing.classes;
 
 
+/** 
+ * Person.java - represents a person. This is a class with properties (things
+ * about the class, adjective, descriptors) and methods (things it can do, 
+ * verbs, actions). Classes can be created (instantiated) into objects
+ * (which are instances of that class).
+ *
+ * @author Mr. Wachs 
+ * @since March 2023
+*/
 public class Person 
 {
 
