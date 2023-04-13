adee9ed4527c96edfd4d327c61dbece81d7addbd
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-04-13 14:15:53 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

---
+++ src/testing/classes/Athlete.java
@@ -0,0 +1,59 @@
+/** Required package class namespace */
+package testing.classes;
+
+ 
+/**
+ * Athlete.java - represents a Athlete Husky HighSchool Student Person. Using 
+ * the keyword 'implements' in the class signature line, means this class 
+ * "follows the rules" of that interface. This will force the programmer to
+ * 'implement' (write) the abstract methods the were 'suggested' in that
+ * interface. This can be done in NetBeans with the light bulb. You can 
+ * implement ("follow the rules of") MULTIPLE interfaces (this cannot be done
+ * with classes - one class can only inherit/extends from one other class). To
+ * do multiple implementation, use a comma between interfaces.
+ *
+ * @author Mr. Wachs
+ * @since 13-Apr-2023, 2:05:34 PM
+ */
+public class Athlete extends Husky
+{
+
+    /**
+     * Default constructor, set class properties
+     */
+    public Athlete() {
+        
+    }
+     
+    /**
+     * String representation of this object
+     *
+     * @return The object represented as a String
+     */
+    @Override
+    public String toString() {
+        return "Athlete: " + super.toString();
+    }
+   
+    /**
+     * Deep comparison, determines if two objects are "equal" in this context
+     *
+     * @param object the object to compare to
+     * @return the objects are "equal" (true) or not (false)
+     */
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+       
+    /**
+     * a Deep clone, creates a duplicate object using new memory
+     *
+     * @return a "clone" of the object using new memory
+     */
+    @Override
+    public Athlete clone() {
+        return this;
+    }
+    
+}
