3c79daedfcf800399e99431ea79c4373923ff311
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-04-19 13:41:03 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -955,12 +955,25 @@ public class ComputerScience30S
         
         // When using the for loop with arrays, it is even BETTER to use a
         // built-in feature of all arrays! All arrays have a ".length" feature
-        // for example    
+        // for example
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(i + " = " + numbers[i]);
         }
         // Tip: if type "for" TAB, then TAB AGAIN it jump to the ending value
         // So if the array size changes, the loop adapts to it
+        
+        // Array can easily work with methods, the arrays can be sent into a 
+        // method (as one of multiple parameters, or the only parameter), and 
+        // arrays can be outputted (returned) from a method - in other words:
+        // Arrays can be INPUT(s) or OUTPUT from a method
+        
+        output(array);
+        output(numbers);
+        
+        
+        
+        
+        
         
         
         
@@ -979,6 +992,22 @@ public class ComputerScience30S
         
         
         System.out.println("Completed learning arrays!");
+    }
+
+    /**
+     * Outputs an array of integers in a dialog
+     * 
+     * @param array the array of integers
+     */
+    private static void output(int[] array) {
+        // Array can be a parameter (or one of the parameters) of a method
+        String text = "[";
+        // Loop (traverse) through the array and add the contents to the string
+        for (int i = 0; i < array.length; i++) {
+            text = text + array[i] + ",";
+        }
+        text = text + "]";
+        output(text);           // Outputs it by calling an overloaded method
     }
 
 }
@@ -1078,6 +1107,14 @@ public class ComputerScience30S
 // (22) NetBeans can auto comment methods if you type "/**" above the method
 //      signature line, it will add the comment and define what needs to be
 //      commented for the method (using the "JavaDoc" commenting style)
+// (23) All arrays have a ".length" feature built in that you can have NetBeans
+//      autocomplete for you by typing the name of your array, then dot length
+//      (".length") which automatically gives you how many elements are in
+//      the array
+// (24) When writing for loops with array, if you type "for" then press TAB, 
+//      and then press TAB AGAIN it jump to the ending value (which should be 
+//      the ".length" of the array (so if the array size changes, the loop 
+//      adapts to it) - the loop variable "i" also represents the array "index"
 
 // </editor-fold>
 
