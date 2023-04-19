f33286960d8d1344d6fb8f8038dbebb6de957ee8
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-04-19 10:30:28 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -945,7 +945,7 @@ public class ComputerScience30S
         
         // Specifically, the FOR loop!
         
-        // "for" loops were literally built for arrays
+        // for loops were literally built for arrays
         for (int i = 0; i < 100; i++) {
             System.out.println(numbers[i]);
         }
@@ -953,8 +953,14 @@ public class ComputerScience30S
         // and TAB) starts at 0, goes up by 1, is "less than" then ending 
         // number (which can be the length)
         
-        
-        
+        // When using the for loop with arrays, it is even BETTER to use a
+        // built-in feature of all arrays! All arrays have a ".length" feature
+        // for example    
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + " = " + numbers[i]);
+        }
+        // Tip: if type "for" TAB, then TAB AGAIN it jump to the ending value
+        // So if the array size changes, the loop adapts to it
         
         
         
