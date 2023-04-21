9376aac0d3d9599ebaec593887eae7322d4f00cc
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-04-21 13:49:23 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/ComputerScience30S.java
+++ src/computerscience30s/ComputerScience30S.java
@@ -1086,6 +1086,17 @@ public class ComputerScience30S
         return numbers;                     // Return completed array
     }
 
+    /**
+     * Generates a random character between tow low and high characters
+     * 
+     * @param low lowest character in the range
+     * @param high highest character in the range
+     * @return random character in range
+     */
+    private static char random(char low, char high) {
+        return (char)random((int)low, (int)high);     // cast to int and back  
+    }
+    
 }
 
 
