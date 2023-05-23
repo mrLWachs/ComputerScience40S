101a9d989e2f584e686e353ad75322812f4d036d
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-05-23 11:27:23 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/Student.java
+++ src/computerscience30s/Student.java
@@ -42,21 +42,28 @@ public class Student extends Person
      */
     public Student(String name, int age, String gender, int number) {
         // You can call the contructor methods of the super class (the parent 
-        // class) from this class (the child or subclass) by agaiin using the
-        // keyword super and round brackets
+        // class) from this class (the child or subclass) by using the keyword
+        // super and round brackets
         super(name, age, gender);
         
-        // These lines of code are no longer needed...
-        /*
-        // The keyword "super" in java means "the super class" or the name
-        // of the class that this class inherts from (in this case "Person")
-        super.name = name;
-        super.gender = gender;
-        // super.age = age;
-        // Still cannot access private/encapsulated properties (or methods)
-        */
-                
+        // We started the code for this constructor with the code you can see 
+        // below first. But, we encountered a few issues (like the age being 
+        // encapsulated / private, etc.) so instead, we ended up making a 
+        // call to the "super-constructor" method of the Person class (see the
+        // line above). I have left this code (but commented it out) as a  
+        // learning reference...
+        
+//        super.name = name;
+//        // The keyword "super" in java means "the super class" or the name
+//        // of the class that this class inherts from (in this case "Person")
+//        super.gender = gender;
+//        // super.age = age;
+//        // You still cannot access private/encapsulated properties (or methods)
+                        
+        // Other class proerties for the Student class specifically still need
+        // to be set to starting values...
         this.number = number;
+        // The keyword "this" can still be used to avoid a name conflict
         gpa  = 0.0;
         book = new Book();
         // The "Book" object must still be instantiated (so it is not "null")
