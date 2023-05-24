6499558f330fb0d906b5d413891cac58953e56eb
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-05-24 10:48:45 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/computerscience30s/Teacher.java
+++ src/computerscience30s/Teacher.java
@@ -1,9 +1,43 @@
+
 /** Required package class namespace */
 package computerscience30s;
 
-
-public class Teacher 
+/**
+ * Teacher.java - represents a Teacher. This class uses inheritance to inherit
+ * the properties and methods from the Person class. This class also 
+ * "associates" with the Student class by making a reference to them within 
+ * the class. This is sometimes simplified as a "has a" relationship.
+ *
+ * @author Mr. Wachs
+ * @since May 24, 2023
+ */
+public class Teacher extends Person
 {
 
+    /**
+     * A public property, an "association" or "composition" ("has a") 
+     * relationship to the Student class using an array
+     */
+    public Student[] students;
+    
+    
+    /**
+     * Constructor for the class sets class properties
+     * 
+     * @param name the name for this Teacher Person
+     */
+    public Teacher(String name) {
+        super(name);
+        students = new Student[100];
+        // We still need to give a size (or instantiate) to the array
+    }
+    
+    /**
+     * Will (1) force every student to study, and (2) make every student talk, 
+     * and (3) have the teacher talk 
+     */
+    public void teach() {
+        
+    }
     
 }
