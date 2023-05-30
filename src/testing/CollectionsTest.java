b15d911f16db636719ad1617513da609d1e521d2
lawrence.wachs <lawrence.wachs@28B5CST01.sjsd.lcl>
2023-05-30 11:02:00 -0500

    Merge origin/Final2023
    Conflicts:
    	src/computerscience30s/ComputerScience30S.java

--- src/testing/CollectionsTest.java
+++ src/testing/CollectionsTest.java
@@ -2,7 +2,10 @@
 /** Required package class namespace */
 package testing;
 
- 
+/** Required imports */
+import collections.Node;
+
+
 /**
  * CollectionsTest.java - tests the concepts learned in this unit.
  *
@@ -16,6 +19,72 @@ public class CollectionsTest
      * Default constructor, set class properties
      */
     public CollectionsTest() {
+        System.out.println("\nCollections testing begins...\n");
+        
+        // Create some node objects...
+        System.out.println("\nTesting constructors.........................\n");
+        Node<String> a = new Node<>();
+        Node<String> b = new Node<>("Sass");
+        Node<String> c = new Node<>("Pros", b);
+        Node<String> d = new Node<>("Joes", c, b);
+        
+        // Output our nodes...
+        System.out.println("\nTesting toString()...........................\n");
+        System.out.println("Node a = " + a.toString());
+        System.out.println("Node b = " + b.toString());
+        System.out.println("Node c = " + c.toString());
+        System.out.println("Node d = " + d.toString());
+        
+        // Clone a node...
+        System.out.println("\nTesting clone()..............................\n");
+        Node<String> e = a.clone();
+        Node<String> f = b.clone();
+        Node<String> g = c.clone();
+        Node<String> h = d.clone();
+        System.out.println("Node e = " + e.toString());
+        System.out.println("Node f = " + f.toString());
+        System.out.println("Node g = " + g.toString());
+        System.out.println("Node h = " + h.toString());
+        
+        // Check if nodes are equal...
+        System.out.println("\nTesting equal()..............................\n");
+        System.out.println("Node a == b? -> " + a.equals(b));
+        System.out.println("Node b == c? -> " + b.equals(c));
+        System.out.println("Node c == d? -> " + c.equals(d));
+        System.out.println("Node d == d? -> " + d.equals(d));
+        System.out.println("Node e == a? -> " + e.equals(a));
+        System.out.println("Node f == b? -> " + f.equals(b));
+        System.out.println("Node g == c? -> " + g.equals(c));
+        System.out.println("Node h == d? -> " + h.equals(d));
+        
+        // finalize some nodes (wipe em out, "destroy" them, free up memory)
+        System.out.println("\nTesting finalize()...........................\n");
+        System.out.println("Node a = " + a.toString());
+        System.out.println("Node b = " + b.toString());
+        System.out.println("Node c = " + c.toString());
+        System.out.println("Node d = " + d.toString());
+        System.out.println("Node e = " + e.toString());
+        System.out.println("Node f = " + f.toString());   
+        System.out.println("Node g = " + g.toString());   
+        System.out.println("Node h = " + h.toString());   
+        a.finalize();
+        b.finalize();
+        c.finalize();
+        d.finalize();
+        e.finalize();
+        f.finalize();  
+        g.finalize();  
+        h.finalize();  
+        System.out.println("Node a = " + a.toString());
+        System.out.println("Node b = " + b.toString());
+        System.out.println("Node c = " + c.toString());
+        System.out.println("Node d = " + d.toString());
+        System.out.println("Node e = " + e.toString());
+        System.out.println("Node f = " + f.toString());
+        System.out.println("Node g = " + g.toString());
+        System.out.println("Node h = " + h.toString());
+        
+        // Create a LinkedList object to test the constructor
         
     }
      
