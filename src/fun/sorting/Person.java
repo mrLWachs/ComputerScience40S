fb1e86444dffcd585b91e9e87a1d4729077fa4e6
lawrence.wachs <lawrence.wachs@28b5cst01.sjsd.lcl>
2024-03-21 15:05:44 -0500

    Merge origin/Samantha into example2024
    Conflicts:
    	outputTestData.txt
    	src/testing/Tester.java
    	src/testing/cs42sap/curriculum/CurriculumContentCS42SAP.java
    	src/testing/cs42sap/curriculum/Part1.java
    	src/testing/cs42sap/curriculum/units/APUnit01PrimitiveTypes.java
    	src/testing/cs42sap/curriculum/units/APUnit03BooleanExpressionsAndIfStatements.java
    	src/testing/cs42sap/curriculum/units/APUnit04Iteration.java

--- src/fun/sorting/Person.java
+++ src/fun/sorting/Person.java
@@ -5,29 +5,40 @@ package fun.sorting;
 public class Person
 {
     
-    public static boolean byName;
+    public static int sortType;
+    
+    public static final int BY_NAME = 1;
+    public static final int BY_AGE  = 2;
+    public static final int BY_IQ   = 3;
+       
     
     public String name;
     public int age;
+    public double iq;
     
-    public Person(String name, int age) {
+    public Person(String name, int age, double iq) {
         this.name = name;
         this.age  = age;
+        this.iq  = iq;
     }
     
     public int compareTo(Person that) {
-        if (byName) {
+        if (sortType == BY_NAME) {
             return this.name.compareTo(that.name);
         }
-        else {
+        else if (sortType == BY_AGE) {
             if (this.age >  that.age) return 1;
             if (this.age <  that.age) return -1;
+        }
+        else if (sortType == BY_IQ) {
+            if (this.iq >  that.iq) return 1;
+            if (this.iq <  that.iq) return -1;
         }
         return 0;
     }
     
     public String toString() {
-        return name + " " + age;
+        return name + "\t" + age + "\t" + iq;
     }
     
 }
--- src/fun/sorting/SortPeople.java
+++ src/fun/sorting/SortPeople.java
@@ -1,6 +1,13 @@
 /** Required package class namespace */
 package fun.sorting;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -22,31 +29,106 @@ public class SortPeople
         if (!shouldRun) {
             return;
         }       
-        final int MAX = 100;
+        
+        final int MAX = 2000;
         ArrayList<Person> people = new ArrayList<>();
         for (int i = 0; i < MAX; i++) {
-            people.add(new Person(getName(),getAge()));
+            people.add(new Person(getName(),getAge(),getIQ()));
         }
+        Person.sortType = Person.BY_NAME;
+        sort(people);
         output(people);
-        Person.byName = true;
+        
+        System.out.println("...............................");
+        
+        Person.sortType = Person.BY_AGE;
+        sort(people);
+        output(people);
+        
+        System.out.println("...............................");
+        
+        Person.sortType = Person.BY_IQ;
         sort(people);
         output(people);
     }
     
-    public static String getName() {
-        return "Nicey Persony";
+    public String getName() {
+        ArrayList<String> names = getNames();
+        if (names != null) {
+            double low  = 0d;
+            double high = (double)(names.size()-1);
+            int index = (int)random(low,high);
+            return names.get(index);
+        }
+        return null;
     }
 
-    private static int getAge() {
-        return (int)(120d * Math.random() + 1d);
+    private int getAge() {
+        return (int)random(1d, 120d);
     }
 
-    private static void output(ArrayList<Person> people) {
-        System.out.println(people.toString());
+    private double getIQ() {
+        return random(0.0,4.0);
+    }
+    
+    private void output(ArrayList<Person> people) {
+        for (Person person : people) {
+            System.out.println(person);
+        }
     }
 
-    private static void sort(ArrayList<Person> people) {
-        // To do...
+    private void sort(ArrayList<Person> list) {
+        if (list == null) return;                   // error check
+        boolean sorted = true;                      // flag to stop or not
+        for (int i = list.size()-1; i >= 0; i--) {  // traverse list
+            sorted = true;                          // assume sorted
+            for (int j = 0; j < i; j++) {           // traverse again
+                Person item1 = list.get(j);  
+                Person item2 = list.get(j+1);
+                if (item1.compareTo(item2) > 0) {   // out of order
+                    sorted = false;                 // flag no sorted
+                    list.set(j, item2);             // swap positions
+                    list.set(j+1, item1);
+                } 
+            }
+            if (sorted) return;                     // return early
+        }
+    }
+    
+    private double random(double low, double high) {
+        double seed   = Math.random();              // Create random seed value
+        double L      = (double)low;                // Cast low boundry
+        double H      = (double)high;               // Cast high boundry
+        double value  = (H - L + 1) * seed + L;     // Apply formula
+        return value;                              // return final value     
+    }
+    
+    private ArrayList<String> getNames() {
+        try {           
+            URL url = getClass().getResource("/fun/sorting/names.txt");
+            URI uri = url.toURI();
+            File fileToOpen = new File(uri);
+            FileReader     stream = new FileReader(fileToOpen);
+            BufferedReader file   = new BufferedReader(stream);
+            ArrayList<String> names = new ArrayList<>();
+            String line = file.readLine();
+            while (line != null) {
+                names.add(line);
+                line = file.readLine();
+            }
+            file.close();
+            return names;
+        }
+        catch (NullPointerException error) {
+            System.out.println("Null: " + error.toString());
+        }
+        catch (IOException error) {
+            System.out.println("File open: " + error.toString());
+        } 
+        catch (URISyntaxException error) {
+            System.out.println("File open: " + error.toString());
+        }
+        return null;
     }
 
 }
